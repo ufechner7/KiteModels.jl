0731a21118aa763a58fef122d1e6832cebac6c6b
Uwe Fechner <fechner@aenarete.eu>
2022-04-03 16:04:54 +0200

    Disabled one test

--- test/test_nomad.jl
+++ test/test_nomad.jl
@@ -1,25 +1,23 @@
-using NOMAD
+using NOMAD, LinearAlgebra
 
 function f(x)
-  return x[1]^2 + x[2]^2
-end
-
-function c(x)
-  return 1 - x[1]
+  return Float64[x[1]^2 + x[2]^2, (x[1]-2)^2]
 end
 
 function eval_fct(x)
-  bb_outputs = [f(x), c(x)]
+  bb_outputs = [norm(f(x))]
   success = true
   count_eval = true
   return (success, count_eval, bb_outputs)
 end
 
 pb = NomadProblem(2, # number of inputs of the blackbox
-                  2, # number of outputs of the blackbox
-                  ["OBJ", "EB"], # type of outputs of the blackbox
+                  1, # number of outputs of the blackbox
+                  ["OBJ"], # type of outputs of the blackbox
                   eval_fct;
                   lower_bound=[-5.0, -5.0],
                   upper_bound=[5.0, 5.0])
+
+pb.options.display_stats = ["BBE", "OBJ", "SOL"] 
 
 result = solve(pb, [3.0, 3.0])
