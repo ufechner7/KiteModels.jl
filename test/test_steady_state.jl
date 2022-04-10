4eb6b450607c6809b88ee2096517307f6f9fd5c3
Uwe Fechner <fechner@aenarete.eu>
2022-04-10 12:31:26 +0200

    Cleanup

--- test/test_steady_state.jl
+++ test/test_steady_state.jl
@@ -1,3 +1,9 @@
+# activate the test environment if needed
+using Pkg
+if ! ("BenchmarkTools" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+
 using Test, BenchmarkTools, StaticArrays, LinearAlgebra, KiteUtils, Plots
 using KiteModels, KitePodModels
 
@@ -9,11 +15,12 @@ if ! @isdefined kps4
 end
 
 const dt = 0.05
+const PLOT = true
 
 function plot2d(x, z; zoom=1)
-    if zoom ==1
-        x_max=maximum(x)
-        z_max=maximum(z)
+    if zoom == 1
+        x_max = maximum(x)
+        z_max = maximum(z)
         plot(x,z, xlabel="x [m]", ylabel="z [m]", legend=false, xlims = (x_max-15.0, x_max+5), ylims = (z_max-15.0, z_max+5))
         plot!([x[7],x[10]],[z[7],z[10]], legend=false)
         plot!([x[8],x[11]],[z[8],z[11]], legend=false)
@@ -46,17 +53,13 @@ function simulate(integrator, steps, plot=false)
     (integrator.p.iter - start) / steps
 end
 
-integrator=KiteModels.init_sim(kps4, 1.0)
-oldpos = deepcopy(kps4.pos)
-kps4.stiffness_factor=0.02
+integrator = KiteModels.init_sim(kps4, 1.0)
+kps4.stiffness_factor = 0.02
 simulate(integrator, 3)
-@time av_steps = simulate(integrator, 100)
-println("Average number of callbacks per time step: $av_steps")
-x = Float64[] 
-z = Float64[]
-for i in 1:length(kps4.pos)
-     push!(x, kps4.pos[i][1])
-     push!(z, kps4.pos[i][3])
+
+if PLOT
+    av_steps = simulate(integrator, 100, true)
+else
+    @time av_steps = simulate(integrator, 100)
 end
-# p = plot2d(x, z; zoom=0)
-# display(p)
+println("Average number of callbacks per time step: $av_steps")
