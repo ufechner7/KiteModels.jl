2cf5d609bb137be66163428b46409c06748c5468
Uwe Fechner <fechner@aenarete.eu>
2022-04-18 11:28:03 +0200

    Add script compare_kps3_kps4.jl

---
+++ test/compare_kps3_kps4.jl
@@ -0,0 +1,70 @@
+using StaticArrays, LinearAlgebra, KiteUtils, Printf
+using KiteModels, KitePodModels, KiteUtils
+
+if ! @isdefined kcu
+    const kcu = KCU(se())
+end
+if ! @isdefined kps4
+    const kps4 = KPS4(kcu)
+end
+if ! @isdefined kps3
+    const kps3 = KPS3(kcu)
+end
+
+
+# the following values can be changed to match your interest
+dt = 0.05
+STEPS = 600
+PLOT = true
+FRONT_VIEW = false
+ZOOM = true
+PRINT = false
+STATISTIC = false
+
+if PLOT
+    using Pkg
+    if ! ("Plots" ∈ keys(Pkg.project().dependencies))
+        using TestEnv; TestEnv.activate()
+    end
+    using Plots
+    include("plot2d.jl")
+end
+
+function simulate(s, integrator, steps, plot=false)
+    start = integrator.p.iter
+    for i in 1:steps
+        if PRINT
+            lift, drag = KiteModels.lift_drag(s)
+            @printf "%.2f: " round(integrator.t, digits=2)
+            println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
+        end
+
+        KiteModels.next_step!(s, integrator)
+        
+        if plot
+            reltime = i*dt
+            if mod(i, 5) == 0
+                p = plot2d(s.pos, reltime; zoom=ZOOM, front=FRONT_VIEW)
+                display(p)                
+            end
+        end
+    end
+    (integrator.p.iter - start) / steps
+end
+
+integrator = KiteModels.init_sim!(kps3, stiffness_factor=0.04, prn=STATISTIC)
+av_steps = simulate(kps3, integrator, STEPS, true)
+
+lift, drag = KiteModels.lift_drag(kps3)
+println("KPS3")
+println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
+println("Average number of callbacks per time step: $av_steps")
+
+kps4.set.alpha_zero = 7.35
+integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.04, prn=STATISTIC)
+av_steps = simulate(kps4, integrator, STEPS, true)
+
+lift, drag = KiteModels.lift_drag(kps4)
+println("KPS4")
+println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
+println("Average number of callbacks per time step: $av_steps")
