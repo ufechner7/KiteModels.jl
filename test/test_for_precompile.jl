ad82fa12b428914b0f76308e6262e678ab115406
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-04-18 07:58:32 +0200

    Fix precompilation

--- test/test_for_precompile.jl
+++ test/test_for_precompile.jl
@@ -17,7 +17,7 @@ let
         start = integrator.p.iter
         for i in 1:steps
             lift, drag = KiteModels.lift_drag(kps4)
-            KiteModels.next_step(kps4, integrator, dt)
+            KiteModels.next_step!(kps4, integrator)
             if kps4.stiffness_factor < 1.0
                 kps4.stiffness_factor+=0.01
             end
@@ -29,7 +29,7 @@ let
         end
         (integrator.p.iter - start) / steps
     end
-    integrator = KiteModels.init_sim(kps4, 1.0, STATISTIC)
+    integrator = KiteModels.init_sim!(kps4, prn=STATISTIC)
     kps4.stiffness_factor = 0.04
     simulate(integrator, 100, true)
 end
