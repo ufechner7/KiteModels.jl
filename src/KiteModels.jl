e441293f142dd09f120308cb805634f85290a228
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-05-08 19:25:18 +0200

    Use linear_solver and max_order from settings

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -326,7 +326,7 @@ function init_sim!(s::AKM; t_end=1.0, stiffness_factor=0.035, prn=false)
     y0, yd0 = KiteModels.find_steady_state!(s; stiffness_factor=stiffness_factor, prn=prn)
 
     differential_vars = ones(Bool, length(y0))
-    solver  = IDA(linear_solver=:GMRES, max_order = 4)
+    solver  = IDA(linear_solver=Symbol(s.set.linear_solver), max_order = s.set.max_order)
     tspan   = (0.0, t_end) 
     abstol  = s.set.abs_tol # max error in m/s and m
     prob    = DAEProblem(residual!, yd0, y0, tspan, s, differential_vars=differential_vars)
