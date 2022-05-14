dfa030e1a5a6f70ab970da18886bfa8807beb423
Uwe Fechner <fechner@aenarete.eu>
2022-05-14 23:27:40 +0200

    Enable all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 cd("..")
 KiteUtils.set_data_path("") 
-# include("test_kps3.jl")
+include("test_kps3.jl")
 include("test_kps4.jl")
-# include("bench3.jl")
-# include("bench4.jl")
+include("bench3.jl")
+include("bench4.jl")
