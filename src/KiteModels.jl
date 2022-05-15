8ea5bd09e57964d7569015bb5db52b10f4b61402
Uwe Fechner <fechner@aenarete.eu>
2022-05-16 01:37:44 +0200

    Fix missing v_reel_out and l_tether

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -328,13 +328,11 @@ function SysState(s::AKM, zoom=1.0)
 
     elevation = calc_elevation(s)
     azimuth = calc_azimuth(s)
-    l_tether = 0.0
-    v_reelout = 0.0
     force = winch_force(s)
     heading = calc_heading(s)
     course = calc_course(s)
     v_app_norm = norm(s.v_apparent)
-    KiteUtils.SysState{P}(s.t_0, orient, elevation, azimuth, l_tether, v_reelout, force, s.depower, s.steering, heading, course, v_app_norm, s.vel_kite, X, Y, Z)
+    KiteUtils.SysState{P}(s.t_0, orient, elevation, azimuth, s.l_tether, s.v_reel_out, force, s.depower, s.steering, heading, course, v_app_norm, s.vel_kite, X, Y, Z)
 end
 
 function calc_pre_tension(s::AKM)
