3e62c17e28b966fd4110e5b1f5678ee18547925d
Uwe Fechner <fechner@aenarete.eu>
2024-03-10 19:28:01 +0100

    cleanup

--- mwes/mwe_01.jl
+++ mwes/mwe_01.jl
@@ -1,6 +1,4 @@
-using StaticArrays, LinearAlgebra, KiteUtils
-
-using KiteModels, KitePodModels
+using KiteUtils, KiteModels, KitePodModels
 
 const SEGMENTS = se().segments
 if ! @isdefined kcu
@@ -10,12 +8,6 @@ if ! @isdefined kps4
     const kps4 = KPS4(kcu)
 end
 
-res1 = zeros(SVector{SEGMENTS+1, KiteModels.KVec3})
-res2 = deepcopy(res1)
-if ! @isdefined res3
-    const res3 = vcat(reduce(vcat, vcat(res1, res2)), zeros(2))
-end
-
-STEPS = 500
 kps4.set.depower = 23.6
 integrator = KiteModels.init_sim!(kps4; stiffness_factor=0.035, prn=false)
+nothing
