84b20b7e425a17820a8cbd4879ae78b001260fd1
Uwe Fechner <fechner@aenarete.eu>
2024-03-10 19:35:38 +0100

    cleanup

--- mwes/mwe_01.jl
+++ mwes/mwe_01.jl
@@ -1,9 +1,6 @@
-using KiteUtils, KiteModels, KitePodModels
+using KiteModels
 
-SEGMENTS::Int64 = se().segments
-kcu::KCU = KCU(se())
-kps4::KPS4 = KPS4(kcu)
+kps4::KPS4 = KPS4(KCU(se()))
 
-kps4.set.depower = 23.6
 integrator = KiteModels.init_sim!(kps4; stiffness_factor=0.035, prn=false)
 nothing
