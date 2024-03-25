56296f38c58d02f904001345b34d1f650390091c
Uwe Fechner <fechner@aenarete.eu>
2024-03-25 18:57:19 +0100

    Bugfix

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -334,7 +334,8 @@ function SysState(s::AKM, zoom=1.0)
     heading = calc_heading(s)
     course = calc_course(s)
     v_app_norm = norm(s.v_apparent)
-    KiteUtils.SysState{P}(s.t_0, orient, elevation, azimuth, s.l_tether, s.v_reel_out, force, s.depower, s.steering, 
+    t_sim = 0
+    KiteUtils.SysState{P}(s.t_0, t_sim, orient, elevation, azimuth, s.l_tether, s.v_reel_out, force, s.depower, s.steering, 
                           heading, course, v_app_norm, s.vel_kite, X, Y, Z)
 end
 
