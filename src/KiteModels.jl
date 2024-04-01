0abe18f6de4b6ba62df145c13423b05584c25817
Uwe Fechner <fechner@aenarete.eu>
2024-04-01 18:06:19 +0200

    set inplace to true

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -453,7 +453,7 @@ function init_sim!(s::AKM; t_end=1.0, stiffness_factor=0.035, prn=false)
     solver  = IDA(linear_solver=Symbol(s.set.linear_solver), max_order = s.set.max_order)
     tspan   = (0.0, t_end) 
     abstol  = s.set.abs_tol # max error in m/s and m
-    prob    = DAEProblem(residual!, yd0, y0, tspan, s, differential_vars=differential_vars)
+    prob    = DAEProblem{true}(residual!, yd0, y0, tspan, s, differential_vars=differential_vars)
     integrator = Sundials.init(prob, solver, abstol=abstol, reltol=s.set.rel_tol)
 end
 
