0568042244ab3071759e089999e557328d07ad90
Uwe Fechner <fechner@aenarete.eu>
2024-04-02 11:21:44 +0200

    add two mwes

---
+++ mwes/mwe_05.jl
@@ -0,0 +1,51 @@
+using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using Sundials, ControlPlots
+
+const G_EARTH  = [0.0, 0.0, -9.81] # gravitational acceleration
+  
+# Example one: Falling mass
+# State vector y   = mass.pos, mass.vel
+# Derivative   yd  = mass.vel, mass.acc
+# Residual     res = (y.vel - yd.vel), (yd.acc - G_EARTH)     
+function res!(res, yd, y, p, time)
+    res[1:3] .= y[4:6] .- yd[1:3]
+    res[4:6] .= yd[4:6] .- G_EARTH 
+end
+
+# Set the initial conditions
+t_final = 10.0              # Final simulation time
+vel_0 = [0.0, 0.0, 50.0]    # Initial velocity
+pos_0 = [0.0, 0.0,  0.0]    # Initial position
+acc_0 = [0.0, 0.0, -9.81]   # Initial acceleration
+y0 = append!(pos_0, vel_0)  # Initial pos, vel
+yd0 = append!(vel_0, acc_0) # Initial vel, acc
+
+differential_vars = ones(Bool, length(y0))
+solver  = IDA(linear_solver=:GMRES, max_order = 4)
+tspan   = (0.0, t_final) 
+abstol  = 0.0006 # max error in m/s and m
+s = nothing
+
+prob    = DAEProblem(res!, yd0, y0, tspan, s, differential_vars=differential_vars)
+sol     = solve(prob, solver; abstol, reltol=0.001)
+prob2   = remake(prob; tspan = (0.0, 2.0))
+
+dt = 0.05
+
+# @time time_, pos_z, vel_z = solve(integrator, dt, t_final)
+
+# # plot the result
+# plt.ax1 = plt.subplot(111) 
+# plt.ax1.set_xlabel("time [s]")
+# plt.plot(time_, pos_z, color="green")
+# plt.ax1.set_ylabel("pos_z [m]")  
+# plt.ax1.grid(true) 
+# plt.ax2 = plt.twinx()  
+# plt.ax2.set_ylabel("vel_z [m/s]")   
+# plt.plot(time_, vel_z, color="red")
+
+# @time time_, pos_z, vel_z = solve(integrator, dt, 2*t_final)
+nothing
---
+++ mwes/mwe_06.jl
@@ -0,0 +1,14 @@
+using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using KiteModels, StaticArrays, LinearAlgebra, BenchmarkTools
+function test(vec)
+    KiteModels.norm(vec)
+end
+vec=MVector{3}([1.0,2,3])
+
+test(vec)
+bytes=@allocated test(vec)
+println("Allocate $bytes bytes!")
+@benchmark test($vec)
