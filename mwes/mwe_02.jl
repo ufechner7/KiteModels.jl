99b162c4e9c98a034f2e28bcfad9cbd95ddaf7d7
Uwe Fechner <fechner@aenarete.eu>
2024-04-02 22:53:05 +0200

    improve mwe_02.jl

--- mwes/mwe_02.jl
+++ mwes/mwe_02.jl
@@ -1,15 +1,17 @@
-using KiteModels, Profile, StatProfilerHTML
+using KiteModels
 
 kps4::KPS4 = KPS4(KCU(se()))
+STEPS = 200
 
 integrator = KiteModels.init_sim!(kps4; stiffness_factor=0.035, prn=false)
+next_step!(kps4, integrator, v_ro = 0, dt=0.05)
 @timev next_step!(kps4, integrator, v_ro = 0, dt=0.05)
 function nsteps(kps4, integrator)
-    for i=1:10
+    for i=1:STEPS
         next_step!(kps4, integrator, v_ro = 0, dt=0.05)
     end
 end
-nsteps(kps4, integrator)
-Profile.clear()
-@profilehtml nsteps(kps4, integrator)
+bytes = @allocated nsteps(kps4, integrator)
+println("Bytes per step: $(Int64(round(bytes/STEPS)))")
 nothing
+# 3.125 ms, 1.175 MB
