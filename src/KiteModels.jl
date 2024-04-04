2f97741780b1516b8023d00107196dce5bd6dcbe
Uwe Fechner <fechner@aenarete.eu>
2024-04-04 13:17:28 +0200

    revert last change

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -465,7 +465,8 @@ function init_sim!(s::AKM; t_end=1.0, stiffness_factor=0.035, prn=false)
     # t_end = 0.05
     tspan   = (0.0, t_end) 
     abstol  = s.set.abs_tol # max error in m/s and m
-    prob    = DAEProblem{true}(residual!, yd0, y0, tspan, s)
+    differential_vars = ones(Bool, length(y0))
+    prob    = DAEProblem{true}(residual!, yd0, y0, tspan, s; differential_vars)
     integrator = OrdinaryDiffEq.init(prob, solver; abstol=abstol, reltol= s.set.rel_tol, save_everystep=false)
 end
 
