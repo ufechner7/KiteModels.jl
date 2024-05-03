32857145c84725d26dcc8fe6c943bbe8ce144d3f
Uwe Fechner <fechner@aenarete.eu>
2024-05-03 13:57:03 +0200

    add mwe_11.jl

---
+++ mwes/mwe_11.jl
@@ -0,0 +1,18 @@
+using Pkg
+if ! ("Plots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using Plots
+Plots.__init__()
+using KiteModels, KitePodModels, KiteUtils
+
+set = deepcopy(se())
+kcu::KCU = KCU(set)
+kps3::KPS3 = KPS3(kcu)
+
+include("../examples/plot2d.jl")
+
+reltime = 0.0
+integrator = KiteModels.init_sim!(kps3, stiffness_factor=0.04)
+p = plot2d(kps3.pos, reltime; zoom=false, front=false, segments=set.segments)
+display(p)   
