ce8325e6d5269483f1ae6ecfd93298ded2a33bee
Uwe Fechner <fechner@aenarete.eu>
2024-05-04 18:16:52 +0200

    Add mwe_11b.jl

--- mwes/mwe_11b.jl
+++ mwes/mwe_11b.jl
@@ -8,10 +8,10 @@ using KiteModels, KitePodModels, KiteUtils
 
 set = deepcopy(se())
 kcu::KCU = KCU(set)
-kps3::KPS3 = KPS3(kcu)
+kps4::KPS4 = KPS4(kcu)
 
 include("../examples/plot2d.jl")
 
 reltime = 0.0
-integrator = KiteModels.init_sim!(kps3, stiffness_factor=0.04)
-plot2d(kps3.pos, reltime; zoom=false, front=false, segments=set.segments)
+integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.5)
+plot2d(kps4.pos, reltime; zoom=false, front=false, segments=set.segments)
