dca6d1214ce610b554cb1b5ce5837aa707f39575
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-06-21 16:51:21 +0200

    use package directly

--- test/3line.jl
+++ test/3line.jl
@@ -1,5 +1,5 @@
-include("../src/KiteModels.jl")
-using .KiteModels
-kps4_3l = KPS4_3L()
+using KiteModels
+kcu = KCU(se())
+kps4_3l = KPS4_3L(kcu)
 integrator = KiteModels.init_sim!(kps4_3l, stiffness_factor=0.04, prn=true, integrator_history=nothing)
 KiteModels.next_step!(kps4_3l, integrator, v_ro=[0.0,0.0,0.0], dt=0.2)
