28cd1a6034a09d0387eab87b855cc8b680f085ba
1-Bart-1 <bart@vandelint.net>
2024-07-01 18:08:16 +0200

    add kps4_3l to precompile workload

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -728,7 +728,7 @@ end
         # they belong to your package or not (on Julia 1.8 and higher)
         integrator = KiteModels.init_sim!(kps3_; stiffness_factor=0.035, prn=false, integrator_history=nothing)
         integrator = KiteModels.init_sim!(kps4_; stiffness_factor=0.035, prn=false, integrator_history=nothing)
-        # integrator = KiteModels.init_sim!(kps4_3l_; stiffness_factor=0.035, prn=false, integrator_history=nothing)
+        integrator = KiteModels.init_sim!(kps4_3l_; stiffness_factor=0.035, prn=false, integrator_history=nothing)
         nothing
     end
 end
