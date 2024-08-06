8590c3aa1b708a5f9e6d44c4599ba9002b8e2c29
Uwe Fechner <fechner@aenarete.eu>
2024-08-06 17:08:02 +0200

    next try

--- test/create_sys_image2.jl
+++ test/create_sys_image2.jl
@@ -8,14 +8,14 @@ if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
     Pkg.add("ControlPlots")
 end
 @info "Loading packages ..."
-using KiteUtils, KitePodModels, KiteModels, ControlPlots, Reexport
+using KiteUtils, KitePodModels, KiteModels, ControlPlots
 using PackageCompiler
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:KiteUtils, :KitePodModels, :KiteModels, :ControlPlots, :Reexport];
+    [:KiteUtils, :KitePodModels, :KiteModels, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
 )
