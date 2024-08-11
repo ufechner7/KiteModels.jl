4b44dcbbbb1c26d66a2f761a5730759fe5810932
Uwe Fechner <fechner@aenarete.eu>
2024-08-11 12:32:54 +0200

    next try

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,10 @@ using Test
 
 cd("..")
 KiteUtils.set_data_path("") 
-include("test_helpers.jl")
 include("test_kps3.jl")
 include("test_kps4.jl")
 include("test_kps4_3l.jl")
 include("bench3.jl")
 include("bench4.jl")
 include("bench4_3l.jl")
+include("test_helpers.jl")
