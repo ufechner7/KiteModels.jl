aadb9a102443c6c7fa2e9c17adb22bc5e7988157
Uwe Fechner <fechner@aenarete.eu>
2024-08-11 12:14:10 +0200

    Cleanup

--- test/test_helpers.jl
+++ test/test_helpers.jl
@@ -4,11 +4,9 @@ using KiteModels
 @testset "Testing KiteModels...." begin
     path=pwd()
     tmpdir=mktempdir()
-    println("tmpdir: ", tmpdir)
     mkpath(tmpdir)
     cd(tmpdir)
     KiteModels.copy_examples()
-    println("Examples: ", readdir(joinpath(tmpdir, "examples")))
     @test isfile(joinpath(tmpdir, "examples", "bench.jl"))
     @test isfile(joinpath(tmpdir, "examples", "compare_kps3_kps4.jl"))
     @test isfile(joinpath(tmpdir, "examples", "menu.jl"))
