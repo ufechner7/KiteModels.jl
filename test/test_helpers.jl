2abb761b6a0e4695836a5acb04a17d712309f5c7
Uwe Fechner <fechner@aenarete.eu>
2024-08-11 12:24:58 +0200

    next try

--- test/test_helpers.jl
+++ test/test_helpers.jl
@@ -19,4 +19,21 @@ using KiteModels
         rm(tmpdir, recursive=true)
     end
     cd(path)
+    path=pwd()
+    tmpdir=mktempdir()
+    mkpath(tmpdir)
+    cd(tmpdir)
+    KiteModels.install_examples()
+    @test isfile(joinpath(tmpdir, "examples", "bench.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "compare_kps3_kps4.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "menu.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "reel_out_1p.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "reel_out_4p.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "reel_out_4p_torque_control.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "simulate_simple.jl"))
+    @test isfile(joinpath(tmpdir, "examples", "simulate_steering.jl"))
+    if ! Sys.iswindows()
+        rm(tmpdir, recursive=true)
+    end
+    cd(path)
 end
