f7ffdc925fffe0d627c6dd99ebd9126c10ad222b
Uwe Fechner <fechner@aenarete.eu>
2024-08-12 11:41:47 +0200

    precompile kps4_3l

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -667,12 +667,14 @@ end
 
     kps4_::KPS4 = KPS4(KCU(se()))
     kps3_::KPS3 = KPS3(KCU(se()))
+    kps4_3l_::KPS4_3L = KPS4_3L(KCU(se(SYS_3L)))
     @assert ! isnothing(kps4_.wm)
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         integrator = KiteModels.init_sim!(kps3_; stiffness_factor=0.035, prn=false)
-        integrator = KiteModels.init_sim!(kps4_; stiffness_factor=0.5, prn=false)       
+        integrator = KiteModels.init_sim!(kps4_; stiffness_factor=0.5, prn=false)     
+        integrator = KiteModels.init_sim!(kps4_3l_; stiffness_factor=0.5, prn=false)   
         nothing
     end
 end
