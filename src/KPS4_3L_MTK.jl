4548b9f14b1f9693cf208fdb21650d65a32bfb6f
Uwe Fechner <fechner@aenarete.eu>
2024-08-16 23:20:19 +0200

    remove dot operators

--- src/KPS4_3L_MTK.jl
+++ src/KPS4_3L_MTK.jl
@@ -99,15 +99,15 @@ function calc_aero_forces_mtk!(s::KPS4_3L, eqs2, force_eqs, force, pos, vel, t, 
 
         eqs2 = [
             eqs2
-            F[:,i] ~ E_c .+ e_y.*cos(α).*s.set.radius .- e_z.*sin(α).*s.set.radius
-            e_r[:,i] ~ (E_c .- F[:,i])./norm(E_c .- F[:,i])
-            y_l[i] ~ cos(α) * s.set.radius
+            F[:,i]          ~ E_c + e_y * cos(α) * s.set.radius - e_z * sin(α) * s.set.radius
+            e_r[:,i]        ~ (E_c - F[:,i]) / norm(E_c - F[:,i])
+            y_l[i]          ~ cos(α) * s.set.radius
             α < π/2 ?
-                v_kite[:,i] ~ ((v_cx .- v_dx)./(y_lc .- y_ld).*(y_l[i] .- y_ld) .+ v_dx) .+ v_cy .+ v_cz :
-                v_kite[:,i] ~ ((v_cx .- v_dx)./(y_lc .- y_ld).*(y_l[i] .- y_ld) .+ v_dx) .+ v_dy .+ v_dz
-            v_a[:,i] ~ s.v_wind .- v_kite[:,i]
-            e_drift[:,i] ~ (e_r[:,i] × e_x)
-            v_a_xr[:,i] ~ v_a[:,i] .- (v_a[:,i] ⋅ e_drift[:,i]) .* e_drift[:,i]
+                v_kite[:,i] ~ ((v_cx - v_dx) / (y_lc - y_ld) * (y_l[i] - y_ld) + v_dx) + v_cy + v_cz :
+                v_kite[:,i] ~ ((v_cx - v_dx) / (y_lc - y_ld) * (y_l[i] - y_ld) + v_dx) + v_dy + v_dz
+            v_a[:,i]        ~ s.v_wind .- v_kite[:,i]
+            e_drift[:,i]    ~ (e_r[:,i] × e_x)
+            v_a_xr[:,i]     ~ v_a[:,i] .- (v_a[:,i] ⋅ e_drift[:,i]) .* e_drift[:,i]
         ]
         if α < π/2
             kite_length[i] = (s.set.tip_length + (s.set.middle_length-s.set.tip_length)*α*s.set.radius/(0.5*s.set.width))
