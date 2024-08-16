315d75923466f9ddec3112552052bbd5288b0422
Uwe Fechner <fechner@aenarete.eu>
2024-08-17 01:43:11 +0200

    refactoring

--- src/KPS4_3L_MTK.jl
+++ src/KPS4_3L_MTK.jl
@@ -300,16 +300,16 @@ function model!(s::KPS4_3L, pos_; torque_control=false)
         pos(t)[1:3, 1:s.num_A] = pos2_ # left right middle
         vel(t)[1:3, 1:s.num_A] = zeros(3, s.num_A) # left right middle
         acc(t)[1:3, 1:s.num_A] = zeros(3, s.num_A) # left right middle
-        tether_length(t)[1:3] = s.tether_lengths
-        steering_pos(t)[1:2] = s.steering_pos
-        steering_vel(t)[1:2] = zeros(2)
-        steering_acc(t)[1:2] = zeros(2)
-        tether_speed(t)[1:3] = zeros(3)   # left right middle
+        tether_length(t)[1:3]  = s.tether_lengths
+        steering_pos(t)[1:2]   = s.steering_pos
+        steering_vel(t)[1:2]   = zeros(2)
+        steering_acc(t)[1:2]   = zeros(2)
+        tether_speed(t)[1:3]   = zeros(3) # left right middle
         segment_length(t)[1:3] = zeros(3) # left right middle
         mass_tether_particle(t)[1:3]      # left right middle
         damping(t)[1:3] = s.set.damping ./ s.tether_lengths ./ s.set.segments   # left right middle
         c_spring(t)[1:3] = s.set.c_spring ./ s.tether_lengths ./ s.set.segments # left right middle
-        P_c(t)[1:3] = 0.5 .* (s.pos[s.num_C]+s.pos[s.num_D])
+        P_c(t)[1:3] = 0.5 .* (s.pos[s.num_C] + s.pos[s.num_D])
         e_x(t)[1:3]
         e_y(t)[1:3]
         e_z(t)[1:3]
@@ -341,26 +341,24 @@ function model!(s::KPS4_3L, pos_; torque_control=false)
     end
 
     # Compute the masses and forces
-    eqs2 = []
-    force_eqs::SizedArray{Tuple{3, s.num_A}, Symbolics.Equation} = SizedArray{Tuple{3, s.num_A}, Symbolics.Equation}(undef)
-    force_eqs[:,:] .= (force[:,:] .~ 0)
+    force_eqs = SizedArray{Tuple{3, s.num_A}, Symbolics.Equation}(undef)
+    force_eqs[:, :] .= (force[:, :] .~ 0)
 
     eqs2 = [
-        eqs2
-        pos[:,s.num_E-2] ~ pos[:,s.num_C] .+ e_z .* steering_pos[1]
-        pos[:,s.num_E-1] ~ pos[:,s.num_D] .+ e_z .* steering_pos[2]
-        vel[:,s.num_E-2] ~ vel[:,s.num_C] .+ e_z .* steering_vel[1]
-        vel[:,s.num_E-1] ~ vel[:,s.num_D] .+ e_z .* steering_vel[2]
-        acc[:,s.num_E-2] ~ acc[:,s.num_C] .+ e_z .* steering_acc[1]
-        acc[:,s.num_E-1] ~ acc[:,s.num_D] .+ e_z .* steering_acc[2]
+        pos[:, s.num_E-2] ~ pos[:, s.num_C] + e_z * steering_pos[1]
+        pos[:, s.num_E-1] ~ pos[:, s.num_D] + e_z * steering_pos[2]
+        vel[:, s.num_E-2] ~ vel[:, s.num_C] + e_z * steering_vel[1]
+        vel[:, s.num_E-1] ~ vel[:, s.num_D] + e_z * steering_vel[2]
+        acc[:, s.num_E-2] ~ acc[:, s.num_C] + e_z * steering_acc[1]
+        acc[:, s.num_E-1] ~ acc[:, s.num_D] + e_z * steering_acc[2]
         segment_length       ~ tether_length  ./ s.set.segments
         mass_tether_particle ~ mass_per_meter .* segment_length
         damping              ~ s.set.damping  ./ segment_length
         c_spring             ~ s.set.c_spring ./ segment_length
-        P_c ~ 0.5 .* (pos[:,s.num_C]+pos[:,s.num_D])
-        e_y .~ (pos[:,s.num_C] .- pos[:,s.num_D]) ./ norm(pos[:,s.num_C] .- pos[:,s.num_D])
-        e_z .~ (pos[:,s.num_E] .- P_c) ./ norm(pos[:,s.num_E] .- P_c)
-        e_x .~ cross(e_y, e_z)
+        P_c ~ 0.5 * (pos[:, s.num_C] + pos[:, s.num_D])
+        e_y ~ (pos[:, s.num_C] - pos[:, s.num_D]) / norm(pos[:, s.num_C] - pos[:, s.num_D])
+        e_z ~ (pos[:, s.num_E] - P_c) / norm(pos[:, s.num_E] - P_c)
+        e_x ~ cross(e_y, e_z)
         rho_kite ~ calc_rho(s.am, pos[3,s.num_A])
     ]
 
@@ -368,12 +366,12 @@ function model!(s::KPS4_3L, pos_; torque_control=false)
     eqs2, force_eqs = inner_loop_mtk!(s, eqs2, force_eqs, t, force, pos, vel, segment_length, c_spring, damping, v_wind_gnd)
     
     for i in 1:3
-        eqs2 = vcat(eqs2, vcat(force_eqs[:,i]))
-        eqs2 = vcat(eqs2, acc[:,i] .~ 0)
+        eqs2 = vcat(eqs2, vcat(force_eqs[:, i]))
+        eqs2 = vcat(eqs2, acc[:, i] .~ 0)
     end
     for i in 4:s.num_E-3
         eqs2 = vcat(eqs2, vcat(force_eqs[:,i]))
-        eqs2 = vcat(eqs2, acc[:,i] .~ [0.0; 0.0; -9.81] .+ (force[:,i] ./ mass_tether_particle[(i-1)%3+1]))
+        eqs2 = vcat(eqs2, acc[:, i] .~ [0.0; 0.0; -9.81] .+ (force[:,i] ./ mass_tether_particle[(i-1)%3+1]))
     end
     for i in s.num_E-2:s.num_E-1
         flap_resistance   = [50.0 * ((vel[:,i]-vel[:, s.num_C]) ⋅ e_z) * e_z[j] for j in 1:3]
