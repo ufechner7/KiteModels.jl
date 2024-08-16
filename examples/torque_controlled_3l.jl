3352ebab491886acc25ca896c562b5ecf2c846ec
Uwe Fechner <fechner@aenarete.eu>
2024-08-16 18:54:32 +0200

    use ControlPlots instead of Plots

--- examples/torque_controlled_3l.jl
+++ examples/torque_controlled_3l.jl
@@ -1,6 +1,11 @@
-using Revise, KiteModels, OrdinaryDiffEq, LinearAlgebra, Plots
+using Revise, KiteModels, OrdinaryDiffEq, LinearAlgebra
 using Base: summarysize
-# using SteadyStateDiffEq
+
+using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using ControlPlots
 
 update_settings()
 set = se("system_3l.yaml")
@@ -9,7 +14,8 @@ set.rel_tol = 0.01
 steps = 150
 dt = 1/set.sample_freq
 tspan   = (0.0, dt) 
-# plot2d([[0,0,0]], 0)
+
+logger = Logger(3*set.segments + 6, steps)
 
 steering = [5,5,-30.0]
 
@@ -17,6 +23,8 @@ steering = [5,5,-30.0]
 println("Running models")
 s = KPS4_3L(KCU(set))
 integrator = KiteModels.init_sim!(s; stiffness_factor=0.1, prn=false, mtk=false, torque_control=true)
+sys_state = KiteModels.SysState(s)
+log!(logger, sys_state)
 
 println("compiling")
 total_new_time = 0.0
@@ -30,6 +38,7 @@ steering_poss = [[],[]]
 reel_out_speedss = [[],[]]
 headings = []
 for i in 1:steps
+    global total_new_time, sys_state
     if i == 1
         global steering = [5,5,-30.0] # left right middle
     end
@@ -39,38 +48,29 @@ for i in 1:steps
     if i == 50
         global steering = [0,10.0,-40]
     end
-    # if i == 10
-    #     global steering = [-0.5,10.5,-0]
-    # end
-    # if i == 20
-    #     global steering = [10.5,10.5,13]
-    # end
-    # println(s.steering_pos, "\t tether_lengths \t", s.reel_out_speeds)
-    heading = calc_heading(s)
-    # println(s.steering_pos)
-    if heading > pi
-        heading -= 2*pi
+
+    if sys_state.heading > pi
+        sys_state.heading -= 2*pi
     end
-    push!(headings, heading)
-    push!(steering_poss[1], s.steering_pos[1])
-    push!(steering_poss[2], s.steering_pos[2])
-    push!(reel_out_speedss[1], s.reel_out_speeds[1])
-    push!(reel_out_speedss[2], s.reel_out_speeds[2])
-    # println(norm.(s.winch_forces))
-    global total_new_time += @elapsed next_step!(s, integrator; set_values=steering)
-    # plot2d(s.pos, i-1; zoom=false, front=true, segments=set.segments)
+    sys_state.var_01 =  s.steering_pos[1]
+    sys_state.var_02 =  s.steering_pos[2]
+    sys_state.var_03 =  s.reel_out_speeds[1]
+    sys_state.var_04 =  s.reel_out_speeds[2]
+    total_new_time += @elapsed next_step!(s, integrator; set_values=steering)
+
+    sys_state = KiteModels.SysState(s)
+    if sys_state.heading > pi
+        sys_state.heading -= 2*pi
+    end
+    log!(logger, sys_state)
 end
-# plot2d(s1.pos, steps; zoom=false, front=false, segments=set.segments)
 
 new_time = (dt*steps) / total_new_time
 println("times realtime new model: ", new_time)
 println("avg steptime new model: ", total_new_time/steps)
 
-Plots.plot(1:steps, [steering_poss, reel_out_speedss, headings], 
-    label=["Steering Pos 1" "Steering Pos 2" "Reel Out Speed 1" "Reel Out Speed 2" "Heading"],
-    linewidth=3)
+plotx(logger.time_vec, [logger.var_01_vec,  logger.var_02_vec], [logger.var_03_vec,  logger.var_04_vec], rad2deg.(logger.heading_vec); 
+      ylabels=["Steering", "Reelout speed", "Heading [deg]"], 
+      labels=[["Steering Pos 1", "Steering Pos 2"], ["v_ro 1", "v_ro 2"], "Heading"], title="Steering and Heading")
 
-# 0.02147723333217222 rad with 500 damping
-# damping was too much...
 
-# 0.14807810376419894 with 50 damping
