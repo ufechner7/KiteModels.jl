571ffaaf3814473f4b37417dba1cae12f103ff59
Uwe Fechner <fechner@aenarete.eu>
2024-08-16 21:19:49 +0200

    add MTK to precompilation

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -5,14 +5,14 @@ if ! ("PackageCompiler" ∈ keys(Pkg.project().dependencies))
     Pkg.update()
 end
 @info "Loading packages ..."
-using Dierckx, StaticArrays, LinearAlgebra, Parameters, NLsolve, DocStringExtensions, Sundials, KiteUtils, KitePodModels, AtmosphericModels, OrdinaryDiffEq, ModelingToolkit
+using Dierckx, StaticArrays, LinearAlgebra, Parameters, NLsolve, DocStringExtensions, Sundials, KiteUtils, KitePodModels, AtmosphericModels, OrdinaryDiffEq, ModelingToolkit, DataInterpolations
 using PackageCompiler
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:Dierckx, :StaticArrays, :Parameters, :NLsolve, :DocStringExtensions, :Sundials, :KiteUtils, :KitePodModels, :AtmosphericModels, :OrdinaryDiffEq, :ModelingToolkit],;
+    [:Dierckx, :StaticArrays, :Parameters, :NLsolve, :DocStringExtensions, :Sundials, :KiteUtils, :KitePodModels, :AtmosphericModels, :OrdinaryDiffEq, :ModelingToolkit, :DataInterpolations],;
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=false,
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
