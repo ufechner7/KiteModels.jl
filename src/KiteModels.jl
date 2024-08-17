c13f87367c77b53e0006f58b271b6494461cc61c
Uwe Fechner <fechner@aenarete.eu>
2024-08-17 15:01:57 +0200

    Update license

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -2,7 +2,7 @@
 
 #= MIT License
 
-Copyright (c) 2020, 2021, 2022, 2024 Uwe Fechner
+Copyright (c) 2020, 2021, 2022, 2024 Uwe Fechner and Bart van de Lint
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
