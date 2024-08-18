6c5aa75b4c497ec5024250e81af170bf3b0c299c
1-Bart-1 <bart@vandelint.net>
2024-08-18 15:40:08 +0200

    add simulate tests

--- test/test_kps4_3l_mtk.jl
+++ test/test_kps4_3l_mtk.jl
@@ -49,7 +49,6 @@ end
 
 set_defaults()
 
-pos1 = nothing
 @testset "test_model!       " begin
     kps4_3l.stiffness_factor = 0.04
     res =  zeros(MVector{6*(kps4_3l.num_A-5)+4+6, SimFloat})
@@ -103,7 +102,7 @@ pos1 = nothing
 
 
     # test step
-    global pos1 = deepcopy(kps4_3l.pos)
+    pos1 = deepcopy(kps4_3l.pos)
     kps4_3l.stiffness_factor = 1.0
     kps4_3l.iter = 0
     KiteModels.set_v_wind_ground!(kps4_3l, calc_height(kps4_3l), kps4_3l.set.v_wind, 0.0)
@@ -115,42 +114,126 @@ pos1 = nothing
     @test all(integrator.ps[kps4_3l.set_values_idx] .== kps4_3l.set_values)
     kps4_3l.t_0 = integrator.t
     OrdinaryDiffEq.step!(integrator, dt, true)
+    @test all(kps4_3l.pos[4:kps4_3l.num_A] .== pos1[4:kps4_3l.num_A])
     update_pos!(kps4_3l, integrator)
     @test all(kps4_3l.pos[4:kps4_3l.num_A] .!= pos1[4:kps4_3l.num_A])
     @test integrator.last_stepfail == false
+    @test kps4_3l.mtk == true
 end
 
+global integrator, pos1
 @testset "test_init         " begin
-    
+    set_defaults()
+    [kps4_3l.pos[i] .= 0 for i in 1:kps4_3l.num_A]
+    global integrator = KiteModels.init_sim!(kps4_3l; stiffness_factor=0.1, prn=false, mtk=true, torque_control=false)
+    @test kps4_3l.mtk == true
+    global pos1 = [
+        [0.0 0.0 0.0]
+        [0.0 0.0 0.0]
+        [0.0 0.0 0.0]
+        [2.826555134916249 0.10162113192687891 8.622005105305957]
+        [2.826555134916249 -0.10162113192687891 8.622005105305957]
+        [2.1298311552238363 0.0 8.131298778702757]
+        [5.456422706641912 0.2038737369459307 17.306023661511617]
+        [5.456422706641912 -0.2038737369459307 17.306023661511617]
+        [4.241908474517871 0.0 16.26723340006626]
+        [7.850932935818464 0.3068255041595917 26.05786267666516]
+        [7.850932935818464 -0.3068255041595917 26.05786267666516]
+        [6.333100132564329 0.0 24.408567603645043]
+        [9.98601991466058 0.4104649244760199 34.87657889907129]
+        [9.98601991466058 -0.4104649244760199 34.87657889907129]
+        [8.401700377570428 0.0 32.55567756458501]
+        [11.842878698197364 0.5147433105579666 43.758044143291855]
+        [11.842878698197364 -0.5147433105579666 43.758044143291855]
+        [10.446505443256301 0.0 40.70879859662597]
+        [13.405815986564694 0.6195842015236318 52.69591805323245]
+        [13.405815986564694 -0.6195842015236318 52.69591805323245]
+        [12.466577313446024 0.0 48.868089171558495]
+        [13.405815986564694 0.6195842015236318 52.69591805323245]
+        [13.405815986564694 -0.6195842015236318 52.69591805323245]
+        [14.171760968247852 1.1102230246251565e-16 52.4606698480763]
+    ]
+    for i in eachindex(kps4_3l.pos)
+        # println(kps4_3l.pos[i]')
+        @test all(pos1[i,:] .≈ kps4_3l.pos[i])
+    end
+    sys_state = KiteModels.SysState(kps4_3l)
+
 end
 
 @testset "test_step         " begin
-    
+    @test kps4_3l.mtk == true
+    KiteModels.next_step!(kps4_3l, integrator)
+    pos2 = [
+        [-5.796369547658875e-19 3.105564228811931e-21 2.048931634380671e-18]
+        [4.1775930164545956e-20 1.2010712923916298e-21 7.298750574041877e-19]
+        [-1.0572099515026155e-18 3.3782511637807305e-19 5.274874242660198e-18]
+        [2.8298579736741147 0.10161329821195392 8.621175820966815]
+        [2.830623570190892 -0.10161108533187954 8.620950345308996]
+        [2.1302535909219413 -1.292341650237965e-5 8.132038539148022]
+        [5.460582249504757 0.20386598364954833 17.305187161613578]
+        [5.461538411996239 -0.20386344965793496 17.304929609827887]
+        [4.242016191052998 -4.1371222398968844e-5 16.26890498869265]
+        [7.855454213540059 0.3068196645628308 26.05717753387838]
+        [7.856489915153269 -0.30681716661732195 26.056923670495657]
+        [6.3316955459341155 -0.00010041801729104318 24.411477480427358]
+        [9.99084415164687 0.4104610182125188 34.87606887722985]
+        [9.991941232468655 -0.41045821413403777 34.8758254139833]
+        [8.397847155693308 -0.00018824714259965992 32.56005879086417]
+        [11.846400321894027 0.5147447186417542 43.75805306941343]
+        [11.847471812019844 -0.5147291067890325 43.75784017580861]
+        [10.440210383536016 -0.0002653046876820298 40.71464210076245]
+        [13.393727106942748 0.619648084377197 52.69888711431324]
+        [13.393746698135939 -0.6195315900226024 52.698882288898126]
+        [12.457316981603986 -0.0003032160793861237 48.87551692338118]
+        [13.394882292641316 0.619648530285251 52.703603694641174]
+        [13.394931457320247 -0.6195311326990197 52.703719616670604]
+        [14.161416203376493 6.691191585922139e-5 52.47012644562449]
+    ]
+    for i in eachindex(kps4_3l.pos)
+        # println(kps4_3l.pos[i]')
+        @test all(pos2[i,:] .≈ kps4_3l.pos[i])
+    end
+    # println(kps4_3l.L_C)
+    @test all(kps4_3l.L_C .≈ [4.302267406329012, 114.92095583517676, 333.75274189991217])
 end
 
 @testset "test_reset        " begin
-    reset_sim!
+    reset_sim!(kps4_3l)
+    reset_time = @elapsed reset_sim!(kps4_3l)
+    @test reset_time < 0.01
+    for i in eachindex(kps4_3l.pos)
+        # println(kps4_3l.pos[i]')
+        @test all(pos1[i,:] .≈ kps4_3l.pos[i])
+    end
+    @test isapprox(kps4_3l.L_C[1], 0.0, atol=1e-6)
+end
+
+function simulate(integrator, steps)
+    for i in 1:steps
+        KiteModels.next_step!(kps4_3l, integrator; set_values=[0.0, 0.0, 0.05])
+    end
+    return integrator.iter/steps
 end
 
 @testset "test_simulate     " begin
-    # STEPS = 50
-
-    # kps4_3l.set.solver = "DFBDF"
-    # # println("finding steady state")
-    # init_50()
-    # integrator = KiteModels.init_sim!(kps4_3l; stiffness_factor=0.035, prn=false)
-    # # println("\nStarting simulation...")
-    # simulate(integrator, STEPS)
-    # av_steps = simulate(integrator, STEPS-10)
-    # if Sys.isapple()
-    #     println("isapple $av_steps")
-    #     @test isapprox(av_steps, 835.25, rtol=0.6)
-    # else
-    #     println("not apple $av_steps")
-    #     @test isapprox(av_steps, 835.25, rtol=0.6)
-    # end
+    STEPS = 30
+    reset_sim!(kps4_3l)
+    # println("\nStarting simulation...")
+    simulate(integrator, STEPS)
+    av_steps = simulate(integrator, STEPS)
+    if Sys.isapple()
+        println("isapple $av_steps")
+        @test isapprox(av_steps, 5.766666666666667, atol=1.0)
+    else
+        println("not apple $av_steps")
+        @test isapprox(av_steps, 5.766666666666667, atol=1.0)
+    end
   
-    # lift, drag = KiteModels.lift_drag(kps4_3l)
+    # println(kps4_3l.L_C)
+    @test all(kps4_3l.L_C .≈ [-1.577566498953694, 168.94195353362062, 456.44628119609695])
+    
+    # @test (normalize(kps4_3l.e_z) - normalize(kps4_3l.L_C))[1] > 0
     # # println(lift, " ", drag) # 703.7699568972286 161.44746368100536
     # @test isapprox(lift, 404.2596735903995, rtol=0.05)
     # sys_state = SysState(kps4_3l)
