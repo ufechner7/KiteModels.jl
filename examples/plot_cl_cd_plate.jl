de2892b200454b788a30f1bb47b96b4cf5147929
Uwe Fechner <fechner@aenarete.eu>
2024-08-20 10:17:49 +0200

    Better plot

--- examples/plot_cl_cd_plate.jl
+++ examples/plot_cl_cd_plate.jl
@@ -1,7 +1,9 @@
 # plot the lift and drag coefficients as function of angle of attack
 # of any of the plates of the kite
 
-using KiteModels
+using KiteModels, LaTeXStrings
+
+set = deepcopy(se())
 
 using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
@@ -18,5 +20,4 @@ for (i, alpha) in pairs(ALPHA)
     CL[i] = KiteModels.calc_cl(alpha)
     CD[i] = KiteModels.calc_cd(alpha)
 end
-display(plot(ALPHA, CL))
-display(plot(ALPHA, CD))
+display(plot(ALPHA, [CL, CD]; xlabel=L"\mathrm{AoA}~\alpha", ylabel="CL, CD", labels=["CL", "CD"]))
