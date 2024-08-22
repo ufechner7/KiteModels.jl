e0195173818298c94f4143a0404771c647221b07
Uwe Fechner <fechner@aenarete.eu>
2024-08-22 18:46:45 +0200

    add function bridle_length

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -669,6 +669,23 @@ function copy_files(relpath, files)
     files
 end
 
+function create_bridle(se)
+    # create the bridle
+    bridle = KiteUtils.get_particles(se.height_k, se.h_bridle, se.width, se.m_k)
+    return bridle
+end
+function bridle_length(se)
+    # calculate the bridle length
+    bridle = create_bridle(se)[2:end]
+    len = norm(bridle[1] - bridle[2])
+    len += norm(bridle[1] - bridle[4])
+    len += norm(bridle[1] - bridle[5])
+    len += norm(bridle[3] - bridle[2])
+    len += norm(bridle[3] - bridle[4])
+    len += norm(bridle[3] - bridle[5])
+end
+
+
 """
     copy_bin()
 
