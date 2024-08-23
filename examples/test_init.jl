2e08210ed3088b88a4cb48e353986aeb8cfdc217
Uwe Fechner <fechner@aenarete.eu>
2024-08-23 21:14:00 +0200

    cleanup

--- examples/test_init.jl
+++ examples/test_init.jl
@@ -26,19 +26,6 @@ STATISTIC = false
 DEPOWER = 0.47:-0.005:0.355
 # end of user parameter section #
 
-bridle_length = KiteModels.bridle_length(set)
-println("bridle_length: $bridle_length")
-bridle_area = (set.d_line/2000) * bridle_length
-println("bridle_area: $bridle_area")
-
-function set_tether_diameter!(se, d; c_spring_4mm = 614600, damping_4mm = 473)
-    set.d_tether = d
-    set.c_spring = c_spring_4mm * (d/4.0)^2
-    set.damping = damping_4mm * (d/4.0)^2
-end
-
-set_tether_diameter!(set, set.d_tether)
-
 elev = set.elevation
 i = 1
 set.v_wind = V_WIND # 25
@@ -46,6 +33,6 @@ logger = Logger(set.segments + 5, STEPS)
 
 kcu = KCU(set)
 kps4 = KPS4(kcu)
-integrator = KiteModels.init_sim!(kps4; delta=0.03, stiffness_factor=0.5, prn=STATISTIC)
+integrator = KiteModels.init_sim!(kps4; delta=0.03, stiffness_factor=0.1, prn=STATISTIC)
 lift, drag = lift_drag(kps4)
 println("Lift: $lift, Drag: $drag, Iterations: $(kps4.iter)")
