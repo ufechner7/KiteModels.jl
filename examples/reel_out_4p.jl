c833de155861646a7fdaccfaef732475035674ad
Uwe Fechner <fechner@aenarete.eu>
2024-08-27 00:10:58 +0200

    improve stability

--- examples/reel_out_4p.jl
+++ examples/reel_out_4p.jl
@@ -63,7 +63,7 @@ function simulate(integrator, steps, plot=false)
     iter / steps
 end
 
-integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.5, prn=STATISTIC)
+integrator = KiteModels.init_sim!(kps4; delta=0, stiffness_factor=1, prn=STATISTIC)
 kps4.sync_speed = 0.0
 
 if PLOT
