a3d09b8c6ed19311c159b3025a2dd4ce6f9359f0
Uwe Fechner <fechner@aenarete.eu>
2024-09-13 18:53:14 +0200

    cleanup

--- examples/steering_test.jl
+++ examples/steering_test.jl
@@ -4,8 +4,8 @@ using KiteModels, KitePodModels, KiteUtils
 
 set = deepcopy(load_settings("system_v9.yaml"))
 
-set.abs_tol=0.000006
-set.rel_tol=0.0000001
+set.abs_tol=0.00006
+set.rel_tol=0.000001
 set.elevation = 69.4
 # set.v_steering = 0.2*6
 # set.steering_gain = 10.0
@@ -15,13 +15,15 @@ set.sample_freq = 50
 set.solver="DFBDF" # IDA or DFBDF
 STEPS = 3100
 PLOT = true
-FRONT_VIEW = false
+FRONT_VIEW = true
 ZOOM = true
 PRINT = false
 STATISTIC = false
 # end of user parameter section #
 
 dt = 1/set.sample_freq
+particles = set.segments + 5
+logger::Logger = Logger(particles, STEPS)
 
 kcu::KCU = KCU(set)
 kps4::KPS4 = KPS4(kcu)
@@ -32,6 +34,11 @@ if PLOT
         using TestEnv; TestEnv.activate()
     end
     using ControlPlots
+end
+
+function wrap2pi(angle)
+    num2pi = floor(angle / 2π + 0.5)
+    angle - 2π * num2pi
 end
 
 function simulate(integrator, steps, steering; plot=false)
@@ -64,10 +71,10 @@ function simulate(integrator, steps, steering; plot=false)
 
         KiteModels.next_step!(kps4, integrator; set_speed=0, dt)
         iter += kps4.iter
-        REL_TIME[i] = reltime
-        STEERING[i] = get_steering(kps4.kcu)
-        HEADING[i] = rad2deg(heading)
-        TURN_RATE[i] = rad2deg(heading - last_heading) / dt
+        sys_state = SysState(kps4)
+        sys_state.var_16 = get_steering(kps4.kcu)
+        sys_state.var_15 = rad2deg(heading - last_heading) / dt
+        log!(logger, sys_state)
         
         if plot
             if mod(i, 5) == 1
@@ -78,20 +85,20 @@ function simulate(integrator, steps, steering; plot=false)
 end
 
 SET_STEERING = 0.1:0.1:0.1
-STEERING = zeros(STEPS)
-TURN_RATE = zeros(STEPS)
-HEADING = zeros(STEPS)
-REL_TIME = zeros(STEPS)
 
 for steering in 1*SET_STEERING
     integrator = KiteModels.init_sim!(kps4;  delta=0.0, stiffness_factor=1, prn=STATISTIC)
-    simulate(integrator, STEPS, steering; plot=true)
+    simulate(integrator, STEPS, steering; plot=false)
 end
 
-plotx(REL_TIME, HEADING, TURN_RATE, -STEERING; xlabel="time [s]", ylabels=["heading [°]", "turn rate [deg/s]", "-rel_steering"], fig="Turn rate vs time")
-# plot(REL_TIME, TURN_RATE, -STEERING; xlabel="time [s]", ylabels=["turn rate [deg/s]", "-rel_steering"], fig="Turn rate vs time")
+function plot_steering_vs_turn_rate()
+    lg = load_log("tmp")
+    sl = lg.syslog
+    psi = rad2deg.(wrap2pi.(sl.heading))
+    plot(sl.time, -sl.var_16, sl.var_15; ylabels=["- rel_steering", "turnrate [°/s]"], fig="steering vs turnrate")
+end
 
-# plot(STEERING, SIDE_CL; xlabel="rel_steering [-]", ylabel="side lift coefficient [-]", fig="Side lift coefficient vs steering")
-
-
-
+save_log(logger, "tmp")
+if PLOT
+    plot_steering_vs_turn_rate()
+end
