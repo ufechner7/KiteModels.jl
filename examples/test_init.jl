d836fe54a7bbff7624bd75b7c754fd5b395a1447
Uwe Fechner <fechner@aenarete.eu>
2024-10-04 21:01:52 +0200

    improve test_init.jl

--- examples/test_init.jl
+++ examples/test_init.jl
@@ -1,7 +1,10 @@
 # plot the lift and drag coefficients as function of angle of attack
 
 using Printf
-using KiteModels, KitePodModels, KiteUtils, LinearAlgebra
+# using Pkg
+# pkg"add KiteUtils#main"
+
+using KiteModels, KitePodModels, KiteUtils, LinearAlgebra, Rotations
 
 set = deepcopy(load_settings("system.yaml"))
 
@@ -9,6 +12,7 @@ using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
     using TestEnv; TestEnv.activate()
 end
+
 using ControlPlots
 plt.close("all")
 
@@ -26,10 +30,17 @@ STATISTIC = false
 DEPOWER = 0.47:-0.005:0.355
 # end of user parameter section #
 
+# function calc_heading(s::KPS4; upwind_dir=-π/2)
+#     orientation = orient_euler(s)
+#     elevation = calc_elevation(s)
+#     azimuth = calc_azimuth(s)
+#     KiteUtils.calc_heading(orientation, elevation, azimuth; upwind_dir, respos=false)
+# end
+
 elev = set.elevation
 i = 1
 set.v_wind = V_WIND # 25
-logger = Logger(set.segments + 5, STEPS)
+logger::Logger = Logger(set.segments + 5, STEPS)
 
 kcu::KCU = KCU(set)
 kps4::KPS4 = KPS4(kcu)
@@ -39,3 +50,28 @@ sys_state = KiteModels.SysState(kps4)
 log!(logger, sys_state)
 elev = rad2deg(logger.elevation_vec[end])
 println("Lift: $lift, Drag: $drag, elev: $elev, Iterations: $(kps4.iter)")
+
+q = QuatRotation(sys_state.orient)
+# println(q)
+roll, pitch, yaw = rad2deg.(KiteUtils.quat2euler(q))
+println("roll: ", roll, " pitch: ", pitch, " yaw: ", yaw)
+println("x:", kps4.x) # from trailing edge to leading edge in ENU reference frame
+println("y:", kps4.y) # to the right looking in flight direction
+println("z:", kps4.z) # down
+azimuth = calc_azimuth(kps4)
+println("azimuth: ", round(rad2deg(azimuth), digits = 2), "°")
+
+# print point C and point D
+pos_C, pos_D = kps4.pos[kps4.set.segments+4], kps4.pos[kps4.set.segments+5]
+
+# print alpha2, alpha3, alpha4
+println("alpha2, alpha3, alpha4: ", kps4.alpha_2, " ", kps4.alpha_3, " ", kps4.alpha_4)
+println("heading: ", round(rad2deg(calc_heading(kps4)), digits = 2), "°")
+
+# output on main branch
+# Lift: 1047.1795339611076, Drag: 281.39765463928745, elev: 72.77014, Iterations: 1552
+# roll: -0.0 pitch: 5.755691707832273 yaw: 90.00000250447816
+# x:[-0.9949585608372032, 0.0, 0.10028689952711267]
+# y:[0.0, 1.0, 0.0]
+# z:[-0.10028689952711267, -2.8981421002141166e-19, -0.9949585608372032]
+# alpha2, alpha3, alpha4: 7.546125780476343 10.000000853773646 10.000000853773646
