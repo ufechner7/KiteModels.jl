c45693689cb5de41963f699bc9da0405e232f14f
Uwe Fechner <uwe.fechner.msc@gmail.com>
2024-10-04 23:14:16 +0200

    Stable branch, updated dependencies and examples (#79)
    * improve test_init.jl
    
    * add files to .gitignore
    
    * add files to .gitignore
    
    * add parking_4p.jl
    
    * add KiteUtils
    
    * add kite.obj
    
    * use main version of KiteUtils
    
    * cleanup
    
    * Bump KiteUtils to 0.7.12
    
    * fix Project.toml

--- examples/test_init.jl
+++ examples/test_init.jl
@@ -27,22 +27,10 @@ STATISTIC = false
 DEPOWER = 0.47:-0.005:0.355
 # end of user parameter section #
 
-function quat2euler(q)
-    # Convert quaternion to RotXYZ
-    rot = RotXYZ(q)
-    
-    # Extract roll, pitch, and yaw from RotXYZ
-    roll = rot.theta1
-    pitch = rot.theta2
-    yaw = rot.theta3
-
-    return roll, pitch, yaw
-end
-
 elev = set.elevation
 i = 1
 set.v_wind = V_WIND # 25
-logger = Logger(set.segments + 5, STEPS)
+logger::Logger = Logger(set.segments + 5, STEPS)
 
 kcu::KCU = KCU(set)
 kps4::KPS4 = KPS4(kcu)
@@ -55,13 +43,26 @@ println("Lift: $lift, Drag: $drag, elev: $elev, Iterations: $(kps4.iter)")
 
 q = QuatRotation(sys_state.orient)
 # println(q)
-roll, pitch, yaw = rad2deg.(quat2euler(q))
+roll, pitch, yaw = rad2deg.(KiteUtils.quat2euler(q))
 println("roll: ", roll, " pitch: ", pitch, " yaw: ", yaw)
-println("x:", kps4.x)
-println("y:", kps4.y)
-println("z:", kps4.z)
+println("x:", kps4.x) # from trailing edge to leading edge in ENU reference frame
+println("y:", kps4.y) # to the right looking in flight direction
+println("z:", kps4.z) # down
+azimuth = calc_azimuth(kps4)
+println("azimuth: ", round(rad2deg(azimuth), digits = 2), "°")
 
 # print point C and point D
 pos_C, pos_D = kps4.pos[kps4.set.segments+4], kps4.pos[kps4.set.segments+5]
 
-println(kps4.alpha_2, " ", kps4.alpha_3, " ", kps4.alpha_4)
+# print alpha2, alpha3, alpha4
+println("alpha2, alpha3, alpha4: ", kps4.alpha_2, " ", kps4.alpha_3, " ", kps4.alpha_4)
+println("heading: ", round(rad2deg(calc_heading(kps4)), digits = 2), "°")
+
+# output on main branch
+# Lift: 1047.1795339611076, Drag: 281.39765463928745, elev: 72.77014, Iterations: 1552
+# roll: -0.0 pitch: 5.755691707832273 yaw: 90.00000250447816
+# x:[-0.9949585608372032, 0.0, 0.10028689952711267]
+# y:[0.0, 1.0, 0.0]
+# z:[-0.10028689952711267, -2.8981421002141166e-19, -0.9949585608372032]
+# alpha2, alpha3, alpha4: 7.546125780476343 10.000000853773646 10.000000853773646
+
---
+++ examples_3d/parking_4p.jl
@@ -0,0 +1,93 @@
+using Printf
+
+using Pkg
+if ! ("KiteViewers" ∈ keys(Pkg.project().dependencies))
+    Pkg.activate("examples_3d")
+    pkg"add ControlPlots#main"
+    pkg"add KiteModels#stable"
+end
+using KiteModels, KitePodModels, KiteUtils, Rotations, StaticArrays
+using ControlPlots, KiteViewers
+
+set = deepcopy(se())
+
+# the following values can be changed to match your interest
+dt = 0.05
+set.solver="DFBDF"              # IDA or DFBDF
+set.linear_solver="GMRES"       # GMRES, LapackDense or Dense
+STEPS = 352
+PRINT = false
+STATISTIC = false
+PLOT=true
+UPWIND_DIR2       = -pi/2+deg2rad(10)     # Zero is at north; clockwise positive
+ZOOM = true
+FRONT_VIEW = true
+SHOW_KITE = true
+# end of user parameter section #
+
+kcu::KCU = KCU(set)
+kps4::KPS4 = KPS4(kcu)
+viewer::Viewer3D = Viewer3D(SHOW_KITE)
+
+v_time = zeros(STEPS)
+v_speed = zeros(STEPS)
+v_force = zeros(STEPS)
+heading = zeros(STEPS)
+
+function simulate(integrator, steps, plot=true)
+    iter = 0
+    for i in 1:steps
+        acc = 0.0
+        v_time[i] = kps4.t_0
+        v_speed[i] = kps4.v_reel_out
+        v_force[i] = winch_force(kps4)
+        heading[i] = rad2deg(wrap2pi(calc_heading(kps4)))
+        set_speed = kps4.sync_speed+acc*dt
+        if PRINT
+            lift, drag = KiteModels.lift_drag(kps4)
+            @printf "%.2f: " round(integrator.t, digits=2)
+            println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
+        end
+        steering = 0
+        if i > 200
+            steering = -0.043
+        end
+        set_depower_steering(kps4.kcu, kps4.depower, steering)
+
+        KiteModels.next_step!(kps4, integrator; set_speed, dt, upwind_dir=UPWIND_DIR2)
+        iter += kps4.iter
+        if plot
+            reltime = i*dt-dt
+            if mod(i, 5) == 1
+                plot2d(kps4.pos, reltime; zoom=true, front=FRONT_VIEW, 
+                       segments=set.segments, fig="front_view") 
+                sleep(0.05)           
+            end
+        end
+        sys_state = SysState(kps4)
+        KiteViewers.update_system(viewer, sys_state; scale = 0.08, kite_scale=3)
+    end
+    iter / steps
+end
+
+integrator = KiteModels.init_sim!(kps4, delta=0, stiffness_factor=0.5, prn=STATISTIC)
+
+println("\nStarting simulation...")
+simulate(integrator, STEPS)
+if PLOT
+    p = plotx(v_time[1:STEPS-100], v_speed[1:STEPS-100], v_force[1:STEPS-100]; ylabels=["v_reelout  [m/s]","tether_force [N]"], fig="winch")
+    # display(p)
+end
+lift, drag = KiteModels.lift_drag(kps4)
+println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
+
+println("v_wind: $(kps4.v_wind)")
+println("UPWIND_DIR2: $(rad2deg(UPWIND_DIR2))°")
+pos = pos_kite(kps4)
+println("pos_y: $(round(pos[2], digits=2))")
+# for an  UPWIND_DIR2 of -80°, pos_y must be negative, also v_wind[2] must be negative
+# this is OK
+
+# print heading
+println("heading: $(round(heading[STEPS], digits=2))°")
+plot(v_time, heading; xlabel="time [s]", ylabel="heading [°]", fig="heading")
