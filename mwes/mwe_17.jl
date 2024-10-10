e1d083be5255f6d3cabb4bd1343c059d57af9efa
Uwe Fechner <fechner@aenarete.eu>
2024-10-10 15:01:47 +0200

    add mwe 17

---
+++ mwes/mwe_17.jl
@@ -0,0 +1,45 @@
+# test calculation of the orientation
+import KiteUtils
+using LinearAlgebra, Rotations
+
+# z-y′-x″ (intrinsic rotations) or x-y-z (extrinsic rotations): 
+# the intrinsic rotations are known as: yaw, pitch and roll
+
+# If x, y and z are given in ENU
+# x = [0, 1, 0] y = [0, 0, 1] z = [1, 0, 0] should give roll 90 degrees
+x = [ 0, 1, 0]
+y = [ 0, 0, 1]
+z = [ 1, 0, 0]
+quat2euler(q::AbstractVector) = quat2euler(QuatRotation(q))
+function quat2euler(q::QuatRotation)
+    # Convert quaternion to RotXYZ
+    rot = RotXYZ(q)
+    
+    # Extract roll, pitch, and yaw from RotXYZ
+    roll = rot.theta1
+    pitch = rot.theta2
+    yaw = rot.theta3
+
+    return roll, pitch, yaw
+end
+
+function calc_orient_quat(x, y, z)
+    # reference frame for the orientation: NED
+    ax = [0, 1,  0] # in ENU reference frame this is pointing to the north
+    ay = [1, 0,  0] # in ENU reference frame this is pointing to the east
+    az = [0, 0, -1] # in ENU reference frame this is pointing down
+    rot = KiteUtils.rot3d(x, y, z, ax, ay, az)
+    return rot
+    # q = QuatRotation(rotation)
+    # return Rotations.params(q)
+end
+rot = calc_orient_quat(x, y, z)
+q = QuatRotation(rot)
+roll, pitch, yaw = rad2deg.(quat2euler(q))
+println("--> orient_quat:       roll: ", roll, " pitch:  ", pitch, "  yaw: ", yaw)
+
+q = Rotations.params(q)
+roll, pitch, yaw = rad2deg.(quat2euler(q))
+println("--> orient_quat:       roll: ", roll, " pitch:  ", pitch, "  yaw: ", yaw)
+rot
+
