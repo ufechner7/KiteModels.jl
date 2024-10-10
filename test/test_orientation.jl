a611035e6b7a48d3f49b7820c4cc442dcae44291
Uwe Fechner <fechner@aenarete.eu>
2024-10-10 21:51:07 +0200

    add test_orientation.jl

---
+++ test/test_orientation.jl
@@ -0,0 +1,76 @@
+# test calculation of the orientation, kite pointing to the north and is at zenith
+using LinearAlgebra, Rotations, Test
+
+# x: from trailing edge to leading edge
+# y: to the right looking in flight direction
+# z: down
+
+# If kite (x axis) is pointing to the north, and is at zenith, then in ENUs reference frame:
+# - x = 0, 1, 0
+# - y = 1, 0, 0
+# - z = 0, 0,-1
+# This would be the NED reference frame.
+
+function is_right_handed_orthonormal(x, y, z)
+    if !(norm(x) ≈ 1) || !(norm(y) ≈ 1) || !(norm(z) ≈ 1)
+        return false
+    end
+    if !((x ⋅ y) ≈ 0) || !((y ⋅ z) ≈ 0) || !((z ⋅ x) ≈ 0)
+        return false
+    end
+    return det([x y z]) ≈ 1
+end
+
+"""
+    rot3d(ax, ay, az, bx, by, bz)
+
+Calculate the rotation matrix that needs to be applied on the reference frame (ax, ay, az) to match 
+the reference frame (bx, by, bz).
+All parameters must be 3-element vectors. Both refrence frames must be orthogonal,
+all vectors must already be normalized.
+
+Source: [TRIAD_Algorithm](http://en.wikipedia.org/wiki/User:Snietfeld/TRIAD_Algorithm)
+"""
+function rot3d(ax, ay, az, bx, by, bz)
+    @assert is_right_handed_orthonormal(ax, ay, az)
+    @assert is_right_handed_orthonormal(bx, by, bz)    
+    R_ai = hcat(ax, az, ay)
+    R_bi = hcat(bx, bz, by)
+    return R_bi * R_ai'
+end
+
+function calc_orient_rot(x, y, z)
+    # reference frame for the orientation: NED
+    ax = [0,  1, 0] # in ENU reference frame this is pointing to the north
+    ay = [1, 0, 0]  # in ENU reference frame this is pointing to the east
+    az = [0, 0, -1] # in ENU reference frame this is pointing down
+    rot = rot3d(ax, ay, az, x, y, z)
+    return rot
+end
+
+quat2euler(q::AbstractVector) = quat2euler(QuatRotation(q))
+function quat2euler(q::QuatRotation)
+    # Convert quaternion to RotXYZ
+    rot = RotXYZ(q)
+    
+    # Extract roll, pitch, and yaw from RotXYZ
+    roll = rot.theta1
+    pitch = rot.theta2
+    yaw = rot.theta3
+
+    return roll, pitch, yaw
+end
+
+@testset "test calculation of the orientation, kite pointing to the north and is at zenith" begin
+    x = [0, 1, 0]
+    y = [1, 0, 0]
+    z = [0, 0,-1]
+    @assert is_right_handed_orthonormal(x, y, z)
+    rot = calc_orient_rot(x, y, z)
+    q = QuatRotation(rot)
+    roll, pitch, yaw = rad2deg.(quat2euler(q))
+    @test roll ≈ 0
+    @test pitch ≈ 0
+    @test yaw ≈ 0
+end
+nothing
