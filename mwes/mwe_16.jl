27e1da50f265d5b45f936a8baed14d6787d1fe1a
Uwe Fechner <fechner@aenarete.eu>
2024-10-10 14:14:01 +0200

    update

--- mwes/mwe_16.jl
+++ mwes/mwe_16.jl
@@ -2,6 +2,9 @@
 import KiteUtils
 using LinearAlgebra, Rotations
 
+# z-y′-x″ (intrinsic rotations) or x-y-z (extrinsic rotations): 
+# the intrinsic rotations are known as: yaw, pitch and roll
+
 # If kite (x-axis) is pointing to the west, against the wind, and is at zenith, then:
 # - x = -1, 0, 0
 # - y =  0, 1, 0
@@ -10,6 +13,18 @@ using LinearAlgebra, Rotations
 x = [-1, 0, 0]
 y = [ 0, 1, 0]
 z = [ 0, 0,-1]
+quat2euler(q::AbstractVector) = quat2euler(QuatRotation(q))
+function quat2euler(q::QuatRotation)
+    # Convert quaternion to RotXYZ
+    rot = RotXYZ(q)
+    
+    # Extract roll, pitch, and yaw from RotXYZ
+    roll = rot.theta1
+    pitch = rot.theta2
+    yaw = rot.theta3
+
+    return roll, pitch, yaw
+end
 
 function calc_orient_quat(x, y, z)
     # reference frame for the orientation: NED
@@ -23,11 +38,11 @@ function calc_orient_quat(x, y, z)
 end
 rot = calc_orient_quat(x, y, z)
 q = QuatRotation(rot)
-roll, pitch, yaw = rad2deg.(KiteUtils.quat2euler(q))
+roll, pitch, yaw = rad2deg.(quat2euler(q))
 println("--> orient_quat:       roll: ", roll, " pitch:  ", pitch, "  yaw: ", yaw)
 
 q = Rotations.params(q)
-roll, pitch, yaw = rad2deg.(KiteUtils.quat2euler(q))
+roll, pitch, yaw = rad2deg.(quat2euler(q))
 println("--> orient_quat:       roll: ", roll, " pitch:  ", pitch, "  yaw: ", yaw)
 rot
 
