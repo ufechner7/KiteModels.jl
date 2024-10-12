1549357d9ef9c1dcea2e39e9fed8e42d76c976a4
Uwe Fechner <fechner@aenarete.eu>
2024-10-12 12:06:06 +0200

    update show_kite.jl

--- examples_3d/show_kite.jl
+++ examples_3d/show_kite.jl
@@ -16,8 +16,44 @@ toc()
 
 viewer::Viewer3D = Viewer3D(true);
 segments=6
-state=demo_state_4p(segments+1, 12)
-update_system(viewer, state, kite_scale=0.25)
+state=demo_state_4p(segments+1, 6)
+
+"""
+    is_right_handed_orthonormal(x, y, z)
+
+Returns `true` if the vectors `x`, `y` and `z` form a right-handed orthonormal basis.
+"""
+function is_right_handed_orthonormal(x, y, z)
+    R = [x y z]
+    R*R' ≈ I && det(R) ≈ 1
+end
+
+"""
+    rot3d(ax, ay, az, bx, by, bz)
+
+Calculate the rotation matrix that needs to be applied on the reference frame (ax, ay, az) to match 
+the reference frame (bx, by, bz).
+All parameters must be 3-element vectors. Both refrence frames must be orthogonal,
+all vectors must already be normalized.
+
+Source: [TRIAD_Algorithm](http://en.wikipedia.org/wiki/User:Snietfeld/TRIAD_Algorithm)
+"""
+function rot3d(ax, ay, az, bx, by, bz)
+    @assert is_right_handed_orthonormal(ax, ay, az)
+    @assert is_right_handed_orthonormal(bx, by, bz)    
+    R_ai = [ax az ay]
+    R_bi = [bx bz by]
+    return R_bi * R_ai'
+end
+
+function calc_orient_rot(x, y, z)
+    # reference frame for the orientation: NED
+    ax = @SVector[0, 1, 0] # in ENU reference frame this is pointing to the north
+    ay = @SVector[1, 0, 0] # in ENU reference frame this is pointing to the east
+    az = @SVector[0, 0,-1] # in ENU reference frame this is pointing down
+    rot = rot3d(ax, ay, az, x, y, z)
+    return rot
+end
 
 # z-y′-x″ (intrinsic rotations) or x-y-z (extrinsic rotations): 
 # the intrinsic rotations are known as: yaw, pitch and roll
@@ -37,9 +73,9 @@ z = [0, 0,-1]
 
 # R = Yaw * Pitch * Roll
 
-yaw = deg2rad(0)
+yaw = deg2rad(-90)
 pitch = deg2rad(0)
-roll = deg2rad(-90)
+roll = deg2rad(0)
 
 D1 = RFR.angle_to_dcm(yaw, pitch, roll, :ZYX)
 x4 = D1 * x
@@ -53,3 +89,10 @@ yaw = euler.a1
 pitch = euler.a2
 roll = euler.a3
 println("Yaw: ", rad2deg(yaw), ", Pitch: ", rad2deg(pitch), ", Roll: ", rad2deg(roll))
+
+rot = calc_orient_rot(x4, y4, z4)
+q = QuatRotation(rot)
+roll, pitch, yaw = rad2deg.(quat2euler(q))
+
+state.orient .= Rotations.params(q)
+update_system(viewer, state, kite_scale=0.25)
