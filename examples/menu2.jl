41c79539cd58b6d388902e5dd5ff7e27f8c9fe55
Uwe Fechner <fechner@aenarete.eu>
2024-10-31 12:19:33 +0100

    extend menu2.jl

--- examples/menu2.jl
+++ examples/menu2.jl
@@ -3,6 +3,7 @@ using REPL.TerminalMenus
 options = ["plot_cl_cd_plate = include(\"plot_cl_cd_plate.jl\")",
            "plot_side_cl = include(\"plot_side_cl.jl\")",
            "steering_test_4p = include(\"steering_test_4p.jl\")",
+           "plot_parking_test = include(\"plot_parking_test.jl\")",
            "quit"]
 
 function menu()
