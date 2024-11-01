e92aecb002b2c9d602c494f279c01f4f53a0fe4a
Uwe Fechner <fechner@aenarete.eu>
2024-11-01 14:28:23 +0100

    Next try

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -676,7 +676,9 @@ end
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
     # list = [OtherType("hello"), OtherType("world!")]
-    # set_data_path("data")
+    println("path: ", dirname(pathof(@__MODULE__)))
+    path = dirname(pathof(@__MODULE__))
+    set_data_path(joinpath(path, "..", "data"))
 
     set = se("system.yaml")
     set.kcu_diameter = 0
