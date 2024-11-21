04c0c66f4efc30e98f90420a5a8ce66463144043
Uwe Fechner <fechner@aenarete.eu>
2024-11-21 18:11:18 +0100

    some progress

--- src/KPS4.jl
+++ src/KPS4.jl
@@ -332,7 +332,8 @@ Updates the vector s.forces of the first parameter.
     x = y × z
     s.x .= x; s.y .= y; s.z .= z # save the kite reference frame in the state
 
-    va_yz1 = va_1 - (va_1 ⋅ x) * x
+    # va_yz1 = va_1 - (va_1 ⋅ x) * x
+    va_xz1 = va_1 - (va_1 ⋅ y) * y
     va_xz2 = va_2 - (va_2 ⋅ y) * y
     va_xy3 = va_3 - (va_3 ⋅ z) * z
     va_xy4 = va_4 - (va_4 ⋅ z) * z
@@ -368,7 +369,7 @@ Updates the vector s.forces of the first parameter.
         s.drag_force .= D2 + D3 + D4
         s.forces[s.set.segments + 3] .+= (L2 + D2)
     end
-    f_d = -0.5 * rho * s.set.area * norm(s.set.cmq * s.pitch_rate * s.set.cord_length) * va_yz1
+    f_d = -0.5 * rho * s.set.area * norm(s.set.cmq * s.pitch_rate * s.set.cord_length) * va_xz1
     s.forces[s.set.segments + 2] .+= f_d
     s.forces[s.set.segments + 4] .+= (L3 + D3 -0.5*f_d)
     s.forces[s.set.segments + 5] .+= (L4 + D4 -0.5*f_d)
