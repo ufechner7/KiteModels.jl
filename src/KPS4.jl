ee66ce188ccbb0a8c4c3f72201de63672c457cbd
Uwe Fechner <fechner@aenarete.eu>
2024-11-22 13:36:04 +0100

    fix calculation of f_d

--- src/KPS4.jl
+++ src/KPS4.jl
@@ -369,7 +369,7 @@ Updates the vector s.forces of the first parameter.
         s.drag_force .= D2 + D3 + D4
         s.forces[s.set.segments + 3] .+= (L2 + D2)
     end
-    f_d = -0.5 * rho * s.set.area * norm(s.set.cmq * s.pitch_rate * s.set.cord_length) * va_xz1
+    f_d = -0.5 * rho * s.set.area * norm(va_xz1) * (s.set.cmq * s.pitch_rate * s.set.cord_length) * va_xz1
     s.forces[s.set.segments + 2] .+= f_d
     s.forces[s.set.segments + 4] .+= (L3 + D3 -0.5*f_d)
     s.forces[s.set.segments + 5] .+= (L4 + D4 -0.5*f_d)
