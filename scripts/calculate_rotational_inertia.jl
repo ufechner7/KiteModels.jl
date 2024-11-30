0959e9def87ab741715e7718b88a663347e5daaf
Uwe Fechner <fechner@aenarete.eu>
2024-11-30 22:56:40 +0100

    cleanup

--- scripts/calculate_rotational_inertia.jl
+++ scripts/calculate_rotational_inertia.jl
@@ -1,45 +1,5 @@
 using KiteUtils
 
-function calculate_rotational_inertia(X::Vector, Y::Vector, Z::Vector, M::Vector, around_center_of_mass::Bool=true, 
-    rotation_point::Vector=[0, 0, 0])
-    @assert size(X) == size(Y) == size(Z) == size(M)
-    
-    if around_center_of_mass
-        # First loop to determine the center of mass
-        x_com = y_com = z_com = m_total = 0.0
-        for (x, y, z, m) in zip(X, Y, Z, M)
-            x_com += x * m
-            y_com += y * m
-            z_com += z * m
-            m_total += m 
-        end
-
-        x_com = x_com / m_total
-        y_com = y_com / m_total
-        z_com = z_com / m_total
-    else
-        x_com = rotation_point[begin]
-        y_com = rotation_point[begin+1]
-        z_com = rotation_point[begin+2]
-    end
-
-    Ixx = Ixy = Ixz = Iyy = Iyz = Izz = 0
-
-    # Second loop using the distance between the point and the center of mass
-    for (x, y, z, m) in zip(X .- x_com, Y .- y_com, Z .- z_com, M)
-        Ixx += m * (y^2 + z^2)
-        Iyy += m * (x^2 + z^2)
-        Izz += m * (x^2 + y^2)
-
-        Ixy += m * x * y
-        Ixz += m * x * z
-        Iyz += m * y * z
-    end
-    
-    Ixx, Ixy, Ixz, Iyy, Iyz, Izz
-end
-
-
 function calculate_inertia_for_setting(settings_file::String, include_kcu::Bool=true, around_kcu::Bool=false)
     set_data_path("data")
     set = deepcopy(load_settings(settings_file))
