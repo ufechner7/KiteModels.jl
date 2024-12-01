94ef61a8582135b09100ca0a2e41960779d18906
Uwe Fechner <fechner@aenarete.eu>
2024-12-01 23:14:57 +0100

    fix typo

--- src/KPS4.jl
+++ src/KPS4.jl
@@ -621,7 +621,7 @@ end
 """
     find_steady_state!(s::KPS4; prn=false, delta = 0.01, stiffness_factor=0.035, upwind_dir=-pi/2))
 
-Find an initial equilibrium, based on the inital parameters
+Find an initial equilibrium, based on the initial parameters
 `l_tether`, elevation and `v_reel_out`.
 """
 function find_steady_state!(s::KPS4; prn=false, delta = 0.01, stiffness_factor=0.035, upwind_dir=-pi/2)
