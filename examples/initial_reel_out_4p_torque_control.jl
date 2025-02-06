024ae3d7de1547fadce288c21f545dc333710e6f
Uwe Fechner <fechner@aenarete.eu>
2025-02-06 18:35:00 +0100

    example works

--- examples/initial_reel_out_4p_torque_control.jl
+++ examples/initial_reel_out_4p_torque_control.jl
@@ -4,10 +4,10 @@ using KiteModels, LinearAlgebra, SciMLBase
 set = deepcopy(load_settings("system.yaml"))
 
 # the following values can be changed to match your interest
-dt = 0.05
+dt = 0.010
 set.solver="DFBDF" # IDA or DFBDF
-set.v_reel_out = 0 # initial reel-out speed [m/s]
-STEPS = 4
+set.v_reel_out = 1.0 # initial reel-out speed [m/s]
+STEPS = 3000
 PLOT = true
 FRONT_VIEW = false
 ZOOM = true
@@ -19,6 +19,7 @@ ALPHA_ZERO = 8.8
 set.alpha_zero = ALPHA_ZERO
 set.version = 2
 set.winch_model = "TorqueControlledMachine"
+set.sample_freq = round(1/dt)
 
 kcu::KCU = KCU(set)
 kps4::KPS4 = KPS4(kcu)
@@ -33,6 +34,7 @@ kps4::KPS4 = KPS4(kcu)
 
 v_time = zeros(STEPS)
 v_speed = zeros(STEPS)
+v_set_speed = zeros(STEPS)
 v_force = zeros(STEPS)
 SOL = nothing
 
@@ -45,31 +47,24 @@ function simulate(integrator, steps, plot=false)
             @printf "%.2f: " round(integrator.t, digits=2)
             println("lift, drag  [N]: $(round(lift, digits=2)), $(round(drag, digits=2))")
         end
-        # dforce = 0.0
-        # if kps4.t_0 > 15.0
-        #     dforce = +4.5
-        # end
-        # force = norm(kps4.forces[1])
-        # r = set.drum_radius
-        # n = set.gear_ratio
-        # set_torque = -r/n * force + dforce
-
-        acc = 0.0
+        # force needed to overcome friction
+        dforce = 3.94 * set.v_reel_out
         if kps4.t_0 > 15.0
-            acc = 0.1
+            dforce = +4.5
         end
-        set_speed = kps4.sync_speed+acc*dt
+        force = norm(kps4.forces[1])
+        r = set.drum_radius
+        n = set.gear_ratio
+        set_torque = -r/n * force + dforce
+
         v_time[i] = kps4.t_0
         v_speed[i] = kps4.v_reel_out
         v_force[i] = winch_force(kps4)
 
-        # println("set_torque: $set_torque")
-        # v_time[i] = kps4.t_0
-        # v_speed[i] = kps4.v_reel_out
-        # v_force[i] = winch_force(kps4)
-        KiteModels.next_step!(kps4, integrator; set_speed, dt)
+        KiteModels.next_step!(kps4, integrator; set_torque, dt)
         if ! SciMLBase.successful_retcode(integrator.sol)
             println("Solver failed at time $(integrator.t)")
+            force = norm(kps4.forces[1])
             println("force: $(force)")
             println("v_reel_out: $(kps4.v_reel_out)")
             println("sync_speed: $(kps4.sync_speed)")
@@ -77,13 +72,12 @@ function simulate(integrator, steps, plot=false)
             SOL = integrator.sol
             break
         end
-        kps4.wm.last_set_speed = set_speed
         iter += kps4.iter
         
         if plot
             reltime = i*dt-dt
-            if mod(i, 5) == 1
-                plot2d(kps4.pos, reltime; zoom=ZOOM, front=FRONT_VIEW, xlim=(37, 78), 
+            if mod(i, 10) == 1
+                plot2d(kps4.pos, reltime; zoom=ZOOM, front=FRONT_VIEW, xlim=(37, 88), 
                                         segments=set.segments, fig="side_view")            
             end
         end
@@ -93,7 +87,7 @@ end
 
 kps4.sync_speed = set.v_reel_out
 kps4.wm.last_set_speed = set.v_reel_out
-integrator = KiteModels.init_sim!(kps4; delta=0, stiffness_factor=1, prn=STATISTIC)
+integrator = KiteModels.init_sim!(kps4; delta=0.00015, stiffness_factor=0.8, prn=STATISTIC)
 
 if PLOT
     av_steps = simulate(integrator, STEPS, true)
@@ -112,25 +106,8 @@ println("Average number of callbacks per time step: $(round(av_steps, digits=2))
 
 if PLOT
     p = plotx(v_time, v_speed, v_force; 
-    ylabels=["v_reelout  [m/s]","tether_force [N]"], fig="winch")
+              ylabels=["v_reelout  [m/s]","tether_force [N]"], 
+              fig="winch")
     display(p)
 end
 # savefig("docs/src/reelout_force_4p.png")
-
-# Solver: DFBDF, reltol=0.000001
-# Total simulation time: 1.049 s
-# Simulation speed: 23.83 times realtime.
-# lift, drag  [N]: 545.24, 102.55
-# Average number of callbacks per time step: 625.604
-
-# Solver: DFBDF, reltol=0.001
-# Total simulation time: 0.178 s
-# Simulation speed: 198.04 times realtime.
-# lift, drag  [N]: 633.13, 119.82
-# Average number of callbacks per time step: 75.67
-
-# Solver: IDA
-# Total simulation time: 0.662 s
-# Simulation speed: 37.78 times realtime.
-# lift, drag  [N]: 639.63, 121.71
-# Average number of callbacks per time step: 379.91
