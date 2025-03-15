b338a6f03b0cb59ebd3f1744b06ca7eec96ca039
1-Bart-1 <bart@vandelint.net>
2025-03-15 23:47:14 +0100

    one succesful step

--- src/plotting.jl
+++ src/plotting.jl
@@ -2,8 +2,8 @@
 function plot(sys::PointMassSystem, reltime)
     pos = [sys.points[i].pos_w for i in eachindex(sys.points)]
     seg = [[sys.segments[i].points[1], sys.segments[i].points[2]] for i in eachindex(sys.segments)]
-    ControlPlots.plot2d(pos, seg, reltime; zoom=false, front=false, xlim=(-0, 20), ylim=(40, 60))
-    # ControlPlots.plot2d(pos, seg, reltime; zoom=false, front=true)
+    # ControlPlots.plot2d(pos, seg, reltime; zoom=false, front=false, xlim=(-0, 20), ylim=(40, 60))
+    ControlPlots.plot2d(pos, seg, reltime; zoom=false, front=false)
 end
 
 function plot(s::KPSQ, reltime)
