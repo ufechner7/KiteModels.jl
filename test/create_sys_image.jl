ee47a7c91dccc00dc66c6712f678296c6a591d76
Uwe Fechner <fechner@aenarete.eu>
2025-04-11 07:18:45 +0200

    Include VortextStepMethod in sysimage

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -7,7 +7,7 @@ end
 @info "Loading packages ..."
 using Dierckx, StaticArrays, LinearAlgebra, Parameters, NLsolve, DocStringExtensions, Sundials, KiteUtils, 
       KitePodModels, AtmosphericModels, OrdinaryDiffEqCore, OrdinaryDiffEqBDF, OrdinaryDiffEqSDIRK, ModelingToolkit,
-      DSP, JLD2, Colors, REPL
+      DSP, JLD2, Colors, REPL, VortexStepMethod
 using PackageCompiler
 
 @info "Creating sysimage ..."
@@ -16,7 +16,7 @@ push!(LOAD_PATH,joinpath(pwd(),"src"))
 PackageCompiler.create_sysimage(
     [:Dierckx, :StaticArrays, :Parameters, :NLsolve, :DocStringExtensions, :Sundials, :KiteUtils, 
      :KitePodModels, :AtmosphericModels, :OrdinaryDiffEqCore, :OrdinaryDiffEqBDF, :OrdinaryDiffEqSDIRK,
-     :ModelingToolkit, :DSP, :JLD2, :Colors, :REPL];
+     :ModelingToolkit, :DSP, :JLD2, :Colors, :REPL, :VortexStepMethod];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=true,
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
