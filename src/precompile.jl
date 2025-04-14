f92435d670378c833622e053ba5a55ceab41e952
Uwe Fechner <fechner@aenarete.eu>
2025-04-14 19:13:00 +0200

    Add decompression of the xz file

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,17 @@
+function decompress_binary(infile, outfile; chunksize=4096)
+    open(infile) do input
+        open(outfile, "w") do output
+            stream = XzDecompressorStream(input)
+            while !eof(stream)
+                write(output, read(stream, chunksize))
+            end
+        end
+    end
+end
+
 @setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
-    # list = [OtherType("hello"), OtherType("world!")]
     path = dirname(pathof(@__MODULE__))
     set_data_path(joinpath(path, "..", "data"))
 
@@ -9,12 +19,21 @@
     set.kcu_diameter = 0
     kps4_::KPS4 = KPS4(KCU(set))
     kps3_::KPS3 = KPS3(KCU(se("system.yaml")))
+
+    input_file = joinpath(path, "..", "data", "prob_dynamic_3_seg.bin.default.xz")
+    output_file = joinpath(path, "..", "data", "prob_dynamic_3_seg.bin.default")
+    if isfile(input_file) && ! isfile(output_file)
+        using CodecXz
+        decompress_binary(input_file, output_file)
+    end
+
     @assert ! isnothing(kps4_.wm)
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether(
         # they belong to your package or not (on Julia 1.8 and higher)
         integrator = KiteModels.init_sim!(kps3_; stiffness_factor=0.035, prn=false)
         integrator = KiteModels.init_sim!(kps4_; delta=0.03, stiffness_factor=0.05, prn=false)
+
         if isfile(joinpath(path, "..", "data", "prob_dynamic_3_seg.bin.default"))
             local set
             # Simulation parameters
@@ -45,8 +64,7 @@
 
             # Initialize at elevation
             measure.sphere_pos .= deg2rad.([60.0 60.0; 1.0 -1.0])
-            KiteModels.init_sim!(s, measure; precompile=true)
-            sys = s.sys
+            KiteModels.init_sim!(s, measure; prn=false, precompile=true)
         end
         nothing
     end
