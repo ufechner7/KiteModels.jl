673a5bb287e15503811f48e1401419165a363519
Uwe Fechner <fechner@aenarete.eu>
2025-04-20 15:06:22 +0200

    Remove obsolete packages

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -36,7 +36,7 @@ module KiteModels
 using PrecompileTools: @setup_workload, @compile_workload 
 using Dierckx, Interpolations, Serialization, StaticArrays, LinearAlgebra, Statistics, Parameters, NLsolve,
       DocStringExtensions, OrdinaryDiffEqCore, OrdinaryDiffEqBDF, OrdinaryDiffEqSDIRK, OrdinaryDiffEqNonlinearSolve,
-      NonlinearSolve, FiniteDiff, DifferentiationInterface
+      NonlinearSolve
 import Sundials
 using Reexport, Pkg
 @reexport using VortexStepMethod
--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -7,7 +7,7 @@ end
 @info "Loading packages ..."
 using Dierckx, StaticArrays, LinearAlgebra, Parameters, NLsolve, DocStringExtensions, Sundials, KiteUtils, 
       KitePodModels, AtmosphericModels, OrdinaryDiffEqCore, OrdinaryDiffEqBDF, OrdinaryDiffEqSDIRK, ModelingToolkit,
-      DSP, JLD2, Colors, REPL, VortexStepMethod, NonlinearSolve, FiniteDiff, OrdinaryDiffEqNonlinearSolve, StatsBase
+      DSP, JLD2, Colors, REPL, VortexStepMethod, NonlinearSolve, OrdinaryDiffEqNonlinearSolve, StatsBase
 using PackageCompiler, BenchmarkTools, Documenter
 
 @info "Creating sysimage ..."
@@ -17,7 +17,7 @@ PackageCompiler.create_sysimage(
     [:Dierckx, :StaticArrays, :Parameters, :NLsolve, :DocStringExtensions, :Sundials, :KiteUtils, 
      :KitePodModels, :AtmosphericModels, :OrdinaryDiffEqCore, :OrdinaryDiffEqBDF, :OrdinaryDiffEqSDIRK, 
      :OrdinaryDiffEqNonlinearSolve, :StatsBase, :PackageCompiler, :BenchmarkTools, :Documenter,
-     :ModelingToolkit, :JLD2, :Colors, :REPL, :VortexStepMethod, :NonlinearSolve, :DSP, :FiniteDiff];
+     :ModelingToolkit, :JLD2, :Colors, :REPL, :VortexStepMethod, :NonlinearSolve, :DSP];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=true,
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
