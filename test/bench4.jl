3eb43d15faa0216ad9f06de11e09a758fc56f912
Uwe Fechner <fechner@aenarete.eu>
2025-04-28 14:34:48 +0200

    Bugfix of test for 1.10

--- test/bench4.jl
+++ test/bench4.jl
@@ -126,8 +126,8 @@ msg = String[]
     time = 0.0
     t = @benchmark residual!($res, $yd0, $y0, $kps4, $time)
     push!(msg, ("Mean time residual!:           $(round(mean(t.times), digits=1)) ns"))
-    println("t.memory: ", t.memory)
-    @test t.memory <= 144
+    # println("t.memory: ", t.memory)
+    @test t.memory <= 176 # 144 for Julia 1.11
 
     # time using Python/ Numba: 8.94 µs, time using Julia 1.7.2: 1.6µs, Julia 1.8.0: 1.244µs
     # Julia 1.9 on Ryzen:  816.1 ns
