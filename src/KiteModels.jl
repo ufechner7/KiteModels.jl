5ae7a4c72daf9260d825859bde5ac340d71a9734
Uwe Fechner <fechner@aenarete.eu>
2025-06-12 17:37:14 +0200

    Remove unused parameter from init_sim!, improve doc string

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -541,13 +541,13 @@ function calc_pre_tension(s::AKM)
 end
 
 """
-    init_sim!(s::AKM; t_end=1.0, stiffness_factor=0.5, delta=0.001, upwind_dir=-pi/2, prn=false)
+    init_sim!(s::AKM; stiffness_factor=0.5, delta=0.0001, upwind_dir=-pi/2, 
+                      prn=false) -> OrdinaryDiffEqCore.ODEIntegrator
 
-Initializes the integrator of the model.
+Initializes the integrator of the model (KPS3 and KPS4 only).
 
 Parameters:
 - s:     an instance of an abstract kite model
-- t_end: end time of the simulation; normally not needed
 - stiffness_factor: factor applied to the tether stiffness during initialization
 - delta: initial stretch of the tether during the steady state calculation
 - upwind_dir: upwind direction in radians, the direction the wind is coming from. Zero is at north; 
@@ -555,9 +555,9 @@ Parameters:
 - prn: if set to true, print the detailed solver results
 
 Returns:
-An instance of a DAE integrator.
+An instance of an `ODEIntegrator`.
 """
-function init_sim!(s::AKM; t_end=1.0, stiffness_factor=0.5, delta=0.0001, upwind_dir=-pi/2, prn=false)
+function init_sim!(s::AKM; stiffness_factor=0.5, delta=0.0001, upwind_dir=-pi/2, prn=false)
     clear!(s)
     s.stiffness_factor = stiffness_factor
     
