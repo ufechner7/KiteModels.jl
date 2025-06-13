78553734ad373eeddb417311c3b4b342902e77ba
1-Bart-1 <bart@vandelint.net>
2025-06-13 18:47:14 +0200

    Respect l_tether from settings

--- ext/KiteModelsControlPlotsExt.jl
+++ ext/KiteModelsControlPlotsExt.jl
@@ -7,7 +7,7 @@ using ControlPlots, KiteModels
 
 export plot
 
-function ControlPlots.plot(sys::SystemStructure, reltime; wing_pos=nothing, e_z=zeros(3), zoom=false, front=false)
+function ControlPlots.plot(sys::SystemStructure, reltime; l_tether=50.0, wing_pos=nothing, e_z=zeros(3), zoom=false, front=false)
     pos = [sys.points[i].pos_w for i in eachindex(sys.points)]
     !isnothing(wing_pos) && (pos = [pos..., wing_pos...])
     seg = [[sys.segments[i].point_idxs[1], sys.segments[i].point_idxs[2]] for i in eachindex(sys.segments)]
@@ -18,11 +18,11 @@ function ControlPlots.plot(sys::SystemStructure, reltime; wing_pos=nothing, e_z=
         xlim = (pos[end][2] - 6, pos[end][2]+6)
         ylim = (pos[end][3] - 10, pos[end][3]+2)
     elseif !zoom && !front
-        xlim = (-5, 60)
-        ylim = (-5, 60)
+        xlim = (-5, l_tether+10)
+        ylim = (-5, l_tether+10)
     elseif !zoom && front
-        xlim = (-32.5, 32.5)
-        ylim = (-5, 60)
+        xlim = (-12.5 - 0.5l_tether, 12.5 + 0.5l_tether)
+        ylim = (-5, l_tether+10)
     end
     ControlPlots.plot2d(pos, seg, reltime; zoom, front, xlim, ylim, dz_zoom=0.6)
 end
@@ -32,7 +32,6 @@ function ControlPlots.plot(s::SymbolicAWEModel, reltime; kwargs...)
     pos = s.integrator[s.sys.pos]
     if length(wings) > 0
         wing_pos = [s.integrator[s.sys.wing_pos[i, :]] for i in eachindex(wings)]
-        @show wing_pos
         e_z = [s.integrator[s.sys.e_z[i, :]] for i in eachindex(wings)]
     else
         wing_pos = nothing
@@ -42,7 +41,7 @@ function ControlPlots.plot(s::SymbolicAWEModel, reltime; kwargs...)
     for (i, point) in enumerate(s.system_structure.points)
         point.pos_w .= pos[:, i]
     end
-    plot(s.system_structure, reltime; wing_pos, e_z, kwargs...)
+    plot(s.system_structure, reltime; s.set.l_tether, wing_pos, e_z, kwargs...)
 end
 
 end
