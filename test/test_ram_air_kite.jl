4435cb78364ed02fcefc4a4eeb65acf85d1d837c
1-Bart-1 <bart@vandelint.net>
2025-06-16 17:05:59 +0200

    New xz file

--- test/test_ram_air_kite.jl
+++ test/test_ram_air_kite.jl
@@ -14,7 +14,7 @@ set_data_path(temp_data_path)
 
 # Testing tolerance
 const TOL = 1e-5
-const BUILD_SYS = false
+const BUILD_SYS = true
 
 @testset verbose = true "SymbolicAWEModel MTK Model Tests" begin
     # Initialize model
