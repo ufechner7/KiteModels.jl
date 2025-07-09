71257633eaaee3e08c2dbc23af60b59467cc0b26
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-09 07:12:41 +0200

    Improve test

--- mwes/mwe_28.jl
+++ mwes/mwe_28.jl
@@ -1,7 +1,12 @@
-using KiteModels, Test
+using KiteModels, KiteUtils, Test
 
 set = load_settings("system.yaml")
 kcu::KCU  = KCU(set)
 kps3::KPS3 = KPS3(kcu)
 
-@test kps3 isa AbstractKiteModel
+@testset "KPS3 constructor interface" begin
+    @test kps3 isa KiteUtils.AbstractKiteModel
+    @test kps3.set isa KiteUtils.Settings
+    # kps3.integrator
+end
+nothing
