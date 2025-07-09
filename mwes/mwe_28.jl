1984d06be7fe080df33097ad69dbd2786e428e77
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-09 07:32:42 +0200

    Update tests

--- mwes/mwe_28.jl
+++ mwes/mwe_28.jl
@@ -1,18 +1,18 @@
 using KiteModels, KiteUtils, Test
 
 set = load_settings("system.yaml")
-kcu::KCU  = KCU(set)
-kps3::KPS3 = KPS3(kcu)
-kps4::KPS4 = KPS4(kcu)
+kps3::KPS3 = KPS3(set)
+# kps4::KPS4 = KPS4(set)
+# saw::SymbolicAWEModel = 
 
 @testset "KPS3 constructor interface" begin
     @test kps3 isa KiteUtils.AbstractKiteModel
     @test kps3.set isa KiteUtils.Settings
     @test isnothing(kps3.integrator)
 end
-@testset "KPS4 constructor interface" begin
-    @test kps4 isa KiteUtils.AbstractKiteModel
-    @test kps4.set isa KiteUtils.Settings
-    @test isnothing(kps4.integrator)
-end
+# @testset "KPS4 constructor interface" begin
+#     @test kps4 isa KiteUtils.AbstractKiteModel
+#     @test kps4.set isa KiteUtils.Settings
+#     @test isnothing(kps4.integrator)
+# end
 nothing
