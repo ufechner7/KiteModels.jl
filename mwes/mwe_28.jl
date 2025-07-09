93411dd4dea5ee42963aef924099e51e10735d94
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-09 07:44:46 +0200

    More tests

--- mwes/mwe_28.jl
+++ mwes/mwe_28.jl
@@ -10,15 +10,18 @@ saw::SymbolicAWEModel = SymbolicAWEModel(set)
     @test kps3 isa KiteUtils.AbstractKiteModel
     @test kps3.set isa KiteUtils.Settings
     @test isnothing(kps3.integrator)
+    @test kps3.am isa AtmosphericModel
 end
 @testset "KPS4 constructor interface" begin
     @test kps4 isa KiteUtils.AbstractKiteModel
     @test kps4.set isa KiteUtils.Settings
     @test isnothing(kps4.integrator)
+    @test kps4.am isa AtmosphericModel
 end
 @testset "SymbolicAWEModel constructor interface" begin
     @test saw isa KiteUtils.AbstractKiteModel
     @test saw.set isa KiteUtils.Settings
     @test isnothing(saw.integrator)
+    @test saw.am isa AtmosphericModel
 end
 nothing
