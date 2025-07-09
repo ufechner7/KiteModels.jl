fca348db414a72ba6661f1ee22138a104e4fdc65
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-07-09 07:41:16 +0200

    Add saw tests

--- mwes/mwe_28.jl
+++ mwes/mwe_28.jl
@@ -3,7 +3,8 @@ using KiteModels, KiteUtils, Test
 set = load_settings("system.yaml")
 kps3::KPS3 = KPS3(set)
 kps4::KPS4 = KPS4(set)
-# saw::SymbolicAWEModel = 
+set = load_settings("system_ram.yaml")
+saw::SymbolicAWEModel = SymbolicAWEModel(set)
 
 @testset "KPS3 constructor interface" begin
     @test kps3 isa KiteUtils.AbstractKiteModel
@@ -14,5 +15,10 @@ end
     @test kps4 isa KiteUtils.AbstractKiteModel
     @test kps4.set isa KiteUtils.Settings
     @test isnothing(kps4.integrator)
+end
+@testset "SymbolicAWEModel constructor interface" begin
+    @test saw isa KiteUtils.AbstractKiteModel
+    @test saw.set isa KiteUtils.Settings
+    @test isnothing(saw.integrator)
 end
 nothing
