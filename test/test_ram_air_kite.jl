ffc2ed9c6dee99645d005d88b9378df1255da953
Uwe Fechner <fechner@aenarete.eu>
2025-07-10 22:19:01 +0200

    Next try

--- test/test_ram_air_kite.jl
+++ test/test_ram_air_kite.jl
@@ -27,8 +27,8 @@ const BUILD_SYS = true
     @info "Creating s:"
     @time s = SymbolicAWEModel(set)
 
-    s.set.abs_tol = 1e-3
-    s.set.rel_tol = 1e-3
+    s.set.abs_tol = 1e-4
+    s.set.rel_tol = 1e-4
 
     # Initialize at elevation
     set.elevation = 80.0
