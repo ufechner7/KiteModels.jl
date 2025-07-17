81f957068196911c3471b3fd7af9c64fa2d01f3f
1-Bart-1 <bart@vandelint.net>
2025-07-17 19:24:21 +0200

    Fix errors

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -36,7 +36,7 @@ export KPS3, KPS4, SymbolicAWEModel, KVec3, SimFloat, ProfileLaw, EXP, LOG, EXPL
 export calc_set_cl_cd!, copy_examples, copy_bin, update_sys_state!                     # helper functions
 export clear!, find_steady_state!, residual!                                           # low level workers
 export init!, reinit!, next_step!, init_pos_vel                                        # high level workers
-export pos_kite, calc_height, calc_elevation, calc_azimuth, calc_heading, calc_course, calc_orient_quat, calc_aoa  # getters
+export pos_kite, calc_height, calc_elevation, calc_azimuth, calc_heading, calc_course, calc_orient_quat # getters
 export calc_azimuth_north, calc_azimuth_east
 export winch_force, lift_drag, cl_cd, lift_over_drag, unstretched_length, tether_length, v_wind_kite     # getters
 export calculate_rotational_inertia!
