bd0cca38f13dddfcf44fafb0270ed71c1c97c5b5
1-Bart-1 <bart@vandelint.net>
2025-07-26 23:33:14 +0200

    Import wing

--- src/KiteModels.jl
+++ src/KiteModels.jl
@@ -59,7 +59,7 @@ import LinearAlgebra: norm
 import OrdinaryDiffEqCore: init, step!
 
 # --- SymbolicAWEModels ---
-import SymbolicAWEModel: Wing
+import SymbolicAWEModels: Wing
 export SymbolicAWEModel, linearize!
 export SystemStructure, Point, Segment, Pulley, Winch, Wing, Transform
 export DynamicsType, DYNAMIC, STATIC, BRIDLE, QUASI_STATIC
